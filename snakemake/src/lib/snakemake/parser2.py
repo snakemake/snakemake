75e591bdd5812b6a8264cfa57db37af30e7fa879
johannes.koester@gmail.com <johannes.koester@gmail.com@9dc630ac-28ba-dcf0-b748-d416c9740215>
2011-12-08 20:02:39 +0000

    Parser reimplementation working.

--- snakemake/src/lib/snakemake/parser2.py
+++ snakemake/src/lib/snakemake/parser2.py
@@ -1,8 +1,47 @@
-from token import *
+from tokenize import *
 import tokenize, collections, inspect
 
+class Tokens:
+	''' Recorder for emitted tokens. '''
+	def __init__(self):
+		self._row, self._col = 1, 0
+		self._tokens = []
+		
+	def add(self, token, string = None):
+		''' Add a new token. Maybe a full TokenInfor object (first arg)
+		or a pair of token type (e.g. NEWLINE) and string. '''
+		if string:
+			type = token
+			token = tokenize.TokenInfo(
+				type = type,
+				string = string,
+				start = (self._row, self._col),
+				end = (self._row, self._col + len(string)),
+				line = '')
+		else:
+			# token is an original token that may have a wrong row
+			if token.start[0] < self._row:
+				token = Tokens._setrow(token, self._row)
+		self._tokens.append(token)
+		if token.type in (NEWLINE, NL):
+			self._row += 1
+			self._col = 0
+		else:
+			self._col += len(token.string) + 1
+		
+		return self
+	
+	@staticmethod
+	def _setrow(token, row):
+		''' Force the row of a token to be of the given value. '''
+		return token._replace(start = (row, token.start[1]), 
+			end = (row, token.end[1]))
+	
+	def __iter__(self):
+		return self._tokens.__iter__()
+
 class States:
-	''' A finite automaton that tranlates snakemake tokens into python tokens. '''
+	''' A finite automaton that translates snakemake tokens into python tokens. '''
 	def __init__(self, filename):
 		self.state = self.python
 		self.filename = filename
@@ -13,6 +52,10 @@ class States:
 			output = self.output,
 			run = self.run)
 		self.current_rule = None
+		self.tokens = Tokens()
+	
+	def __iter__(self):
+		return self.tokens.__iter__()
 
 	def _syntax_error(self, msg, token):
 		''' Provide a convenient SyntaxError '''
@@ -20,13 +63,10 @@ class States:
 
 	def python(self, token):
 		''' The automaton state that handles ordinary python code. '''
-		if token.type == NAME:
-			if token.string in ('workdir', 'rule'):
-				self.state = self.main_states[token.string]
-			else:
-				yield token
+		if token.type == NAME and token.string in ('workdir', 'rule'):
+			self.state = self.main_states[token.string]
 		else:
-			yield token
+			self.tokens.add(token)
 
 	def workdir(self, token):
 		''' State that handles workdir definition. '''
@@ -36,7 +76,7 @@ class States:
 	def workdir_path(self, token):
 		''' State that translates the workdir path into a function call. '''
 		if token.type == STRING:
-			for t in States._func(token, '_set_workdir', token.string): yield t
+			self._func('_set_workdir', token.string)
 			self.state = self.python
 		else:
 			raise self._syntax_error('Expected string after workdir keyword', token)
@@ -45,7 +85,7 @@ class States:
 		''' State that handles rule definition. '''
 		if token.type == NAME:
 			self.current_rule = token.string
-			for t in States._func(token, '_add_rule', States._stringify(token.string)): yield t
+			self._func('_add_rule', States._stringify(token.string))
 			self.state = self.rule_colon
 		else:
 			raise self._syntax_error('Expected name after rule keyword.', token)
@@ -56,12 +96,11 @@ class States:
 
 	def rule_body(self, token):
 		''' State that handles the rule body. '''
-		if token.type in (INDENT, DEDENT, NEWLINE):
-			yield token
+		if token.type == NEWLINE:
+			self.tokens.add(token)
 		elif token.type == NAME and token.string in self.main_states:
 			self.state = self.main_states[token.string]
-		else:
-			print(token)
+		elif not token.type in (INDENT, DEDENT):
 			raise self._syntax_error('Expected one of the keywords "input", "output", "run" or "rule" below rule definition', token)
 
 	def input(self, token):
@@ -75,95 +114,90 @@ class States:
 	def inoutput(self, token, type):
 		''' State that handles in- and output definition (depending on type). '''
 		self._check_colon(type, token)
-		for t in States._func_open(token, '_set_{}'.format(type)): yield t
+		self._func_open('_set_{}'.format(type))
+		self.tokens.add(LSQB, '[')
 		self.state = self.inoutput_paths
 		
 	def inoutput_paths(self, token):
 		''' State that collects the arguments for in- or output definition '''
 		if token.type == NAME and token.string in self.main_states:
 			self.state = self.main_states[token.string]
-			yield States._func_close(token)
+			self.tokens.add(RSQB, ']')
+			self._func_close()
 		else:
-			yield token
-
+			self.tokens.add(token)
+			
 	def run(self, token):
 		''' State that creates a run function for the current rule. '''
-		self._check_colon(type, token)
-		for t in States._func_def(token, self.current_rule,
-			['input', 'output', 'wildcards']): yield t
+		self._check_colon('run', token)
+		self._func_def(self.current_rule, ['input', 'output', 'wildcards'])
 		self.state = self.run_body
 
 	def run_body(self, token):
 		''' State that collects the body of a rule's run function. '''
-		if token.type == NAME and token.string in self.main_states:
-			self.state = self.main_states[token.string]
+		if token.type == NAME and token.string == 'rule':
+			self.tokens.add(NEWLINE, '\n')
+			self.state = self.rule
 		else:
-			yield token
+			self.tokens.add(token)
 
 	def _check_colon(self, keyword, token):
 		''' Check wether the token is a colon, else raise a syntax error 
 		for the given keyword. '''
 		if not (token.type == tokenize.OP and token.string == ':'):
-			raise self._syntax_error('Expected ":" after workdir keyword', token)
+			raise self._syntax_error('Expected ":" after {} keyword'.format(keyword), token)
 
-	@staticmethod
-	def _func_def(token, name, args):
+	def _func_def(self, name, args):
 		''' Generate tokens for a function definition with given name 
 		and args. '''
-		yield token._replace(type = NAME, string = 'def')
-		yield token._replace(type = NAME, string = name)
-		yield token._replace(type = LPAR, string = '(')
+		self.tokens.add(NAME, 'def') \
+				   .add(NAME, name) \
+				   .add(LPAR, '(')
 		for i in range(len(args)):
-			yield token._replace(type = NAME, string = arg)
-			if i < len(args) - 1: yield token._replace(type = COMMA, string = ',')
-		yield token._replace(type = RPAR, string = ')')
-		yield token._replace(type = COLON, string = ':')
+			self.tokens.add(NAME, args[i])
+			if i < len(args) - 1: 
+				self.tokens.add(COMMA, ',')
+		self.tokens.add(RPAR, ')') \
+				   .add(COLON, ':')
 
-	@staticmethod
-	def _func(token, name, arg):
+	def _func(self, name, arg):
 		''' Generate tokens for a function invocation with given name 
 		and args. '''
-		token = States._fix_col(token)
-		for t in States._func_open(token, name): yield t
-		yield token._replace(type=STRING, string=arg)
-		yield States._func_close(token)
+		self._func_open(name)
+		self.tokens.add(STRING, arg)
+		self._func_close()
 		
-	@staticmethod
-	def _func_open(token, name):
+	def _func_open(self, name):
 		''' Generate tokens for opening a function invocation with 
 		given name. '''
-		yield token._replace(type = NAME, string = name)
-		yield token._replace(type = LPAR, string = '(')
+		self.tokens.add(NAME, name) \
+				   .add(LPAR, '(')
 
-	@staticmethod
-	def _func_close(token):
+	def _func_close(self):
 		''' Generate tokens for closing a function invocation with 
 		given name. '''
-		return token._replace(type = RPAR, string = ')')
-	
-	@staticmethod
-	def _fix_col(token):
-		return token._replace(start = (token.start[0], 0))
-	
+		self.tokens.add(RPAR, ')') \
+				   .add(NEWLINE, '\n')
+		
 	@staticmethod
 	def _stringify(tokenstring):
-		return '"{}"'.format(tokenstring)
-
+		''' Encapsulate a string into additional quotes. '''
+		return '"{}"'.format(tokenstring)			
 
 def snakemake_to_python(tokens, filepath):
+	''' Translate snakemake tokens into python tokens using 
+	a finite automaton. '''
 	states = States(filepath)
 	for snakemake_token in tokens:
-		python_tokens = states.state(snakemake_token)
-		if inspect.isgenerator(python_tokens):
-			for python_token in python_tokens:
-				yield python_token
+		states.state(snakemake_token)
+	for python_token in states:
+		if not python_token.type in (INDENT, DEDENT):
+			yield python_token
 
 def compile_to_python(filepath):
+	''' Compile a given Snakefile into python code. '''
 	with open(filepath) as snakefile:
 		snakemake_tokens = list(snakemake_to_python(
 			tokenize.generate_tokens(snakefile.readline), filepath))
-		#print(snakemake_tokens)
-		#print(len(snakemake_tokens))
 		compilation = tokenize.untokenize(snakemake_tokens)
-		print(compilation)
 		return compilation
