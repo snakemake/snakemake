3b827cad1cd9ac176086f66b3e7445dea5fac2d7
johannes.koester@gmail.com <johannes.koester@gmail.com@9dc630ac-28ba-dcf0-b748-d416c9740215>
2012-01-31 21:28:40 +0000

    More pipe experiments.

--- snakemake/snakemake/pipetest.py
+++ snakemake/snakemake/pipetest.py
@@ -2,31 +2,35 @@ import inspect
 import sys
 import subprocess as sp
 
-class shell:
+class shell(sp.Popen):
+	processes = []
 	def __init__(self, cmd):
-		self.cmd = cmd
-		self._process = None
-	
-	def _run(self, stdin = None):
-		if not self._process:
-			self._process = sp.Popen(self.cmd, shell=True, stdin = stdin, stdout=sp.PIPE, close_fds=True)
-
-	def wait(self):
-		self._run()
-		self._process.wait()
-
+		super(shell, self).__init__(cmd, shell=True, stdin = sp.PIPE, stdout=sp.PIPE, close_fds=True)
+		shell.processes.append(self)
+		
+	def stdoutlines(self):
+		while True:
+			o = self.stdout.readline()
+			if o == b'' and self.poll() != None:
+				return
+			yield o
+				
 	def __or__(self, other):
-		self._run()
 		if isinstance(other, shell):
-			other._run(stdin = self._process.stdout)
+			for o in self.stdoutlines():
+				other.stdin.write(o)
+			other.stdin.close()
 		else:
-			stdout = self._process.stdout.read().decode("utf-8")
-			if isinstance(other, str):
-				other += stdout
-			else:
-				other.write(stdout)
+			for o in self.stdoutlines():
+				other.write(o.decode("utf-8"))
 		return other
 
 
-#shell("echo b; echo a; echo c > blub")
+shell("echo b; echo a; echo c > blub")
 shell("echo b; echo a; echo c") | shell("sort") | sys.stdout
+
+x = shell("echo 2; echo 1")
+x | shell("sort") | sys.stdout
+
+for p in shell.processes:
+	p.wait()
