d2c86a3e5a8b287269039fb66ec0eeef93fefb81
johannes.koester@gmail.com <johannes.koester@gmail.com@9dc630ac-28ba-dcf0-b748-d416c9740215>
2012-02-01 09:01:38 +0000

    making pipetest threaded.

--- snakemake/snakemake/pipetest.py
+++ snakemake/snakemake/pipetest.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 import sys
 import subprocess as sp
+from threading import Thread
 
 class PipeWriter:
 	def __init__(self, towrite):
@@ -17,19 +18,31 @@ class TextIOWriter(PipeWriter):
 	def write(self, line):
 		self._towrite.write(line.decode("utf-8"))
 
-
 class shell(sp.Popen):
 	processes = []
 	def __init__(self, cmd):
 		super(shell, self).__init__(cmd, shell=True, stdin = sp.PIPE, stdout=sp.PIPE, close_fds=True)
 		shell.processes.append(self)
 		
+	@staticmethod
+	def join():
+		for p in shell.processes:
+			p.wait()
+		shell.processes = []
+		
 	def _stdoutlines(self):
 		while True:
 			o = self.stdout.readline()
-			if o == b'' and self.poll() != None:
+			if not o:
 				return
 			yield o
+
+	def _write_pipes(self, pipes, toclose):
+		for l in self._stdoutlines():
+			for pipe in pipes:
+				pipe.write(l)
+		for pipe in toclose:
+			pipe.close()
 				
 	def __or__(self, other):
 		if not isinstance(other, tuple):
@@ -50,15 +63,11 @@ class shell(sp.Popen):
 				raise UnsupportedOperation("Only shell, files, stdout or lists allowed right to a shell pipe.")
 			pipes.append(writer)
 		
-		for l in self._stdoutlines():
-			for pipe in pipes:
-				pipe.write(l)
-		for pipe in toclose:
-			pipe.close()
+		Thread(target = self._write_pipes, args = (pipes, toclose)).start()
 			
 		if len(other) == 1:
 			return other[0]
-
+	
 
 shell("echo b; echo a; echo c > foo")
 shell("echo b; echo a; echo c") | shell("sort") | sys.stdout
@@ -67,10 +76,12 @@ shell("echo a; echo b") | (shell("sort"), shell("cut -f1"))
 
 x = shell("echo 2; echo 1")
 x | shell("sort") | sys.stdout
+shell.join() # ensure that all shells are finished before next line
+print("test")
 
 y = []
-shell("echo foo; echo bar") | y
+shell("echo foo; echo ''; echo bar") | y
+shell.join() # ensure that all shells are finished before next line
 print(y)
 
-for p in shell.processes:
-	p.wait()
+shell.join()
