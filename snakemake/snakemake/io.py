dda7a10b87cffbc03a31fe8eaa635f324dd78e1a
johannes.koester@gmail.com <johannes.koester@gmail.com@9dc630ac-28ba-dcf0-b748-d416c9740215>
2012-03-06 13:08:17 +0000

    Refactoring.

---
+++ snakemake/snakemake/io.py
@@ -0,0 +1,83 @@
+import os, re
+
+class IOFile(str):
+	wildcard_regex = "\{(?P<name>\w+?)(,(?P<constraint>.*))?\}"
+	_register = dict()
+	
+	@classmethod
+	def create(cls, file, temp = False):
+		obj = None
+		if file in cls._register:
+			obj = cls._register[file]
+		else:
+			obj = IOFile(file)
+			cls._register[file] = obj 
+		if obj._temp == None:
+			obj._temp = temp
+		return obj
+	
+	def __init__(self, file, protected = False):
+		self._file = file
+		self._needed = 0
+		self._temp = None
+		self._protected = protected
+		
+	def need(self):
+		self._needed += 1
+	
+	def used(self):
+		self._needed -= 1
+		if self._temp:
+			os.remove(self._file)
+	
+	def prepare(self):
+		dir = os.path.dirname(self._file)
+		if len(dir) > 0 and not os.path.exists(dir):
+			os.makedirs(dir)
+	
+	def created(self, rulename, lineno, snakefile):
+		if not os.path.exists(self._file):
+			raise MissingOutputException("Output file {} not produced by rule {}.".format(self._file, rulename), lineno = lineno, snakefile = snakefile)
+		if self._protected:
+			mode = os.stat(o).st_mode & ~stat.S_IWUSR & ~stat.S_IWGRP & ~stat.S_IWOTH
+			if os.path.isdir(o):
+				for root, dirs, files in os.walk(o):
+					for d in dirs:
+						os.chmod(os.path.join(o, d), mode)
+					for f in files:
+						os.chmod(os.path.join(o, f), mode)
+			else:
+				os.chmod(o, mode)
+	
+	def remove(self):
+		if os.path.exists(self._file):
+			if os.path.isdir(self._file):
+				for root, dirs, files in os.walk(o):
+					for f in files:
+						os.remove(f)
+				for root, dirs, files in os.walk(o):
+					for d in dirs:
+						os.rmdir(d)
+			else:
+				os.remove(o)
+
+	def apply_wildcards(self, wildcards):
+		return self.create(re.sub(self.wildcard_regex, lambda match: '{}'.format(wildcards[match.group('name')]), self._file))
+		#return self.create(f.format(**wildcards))
+		
+	def get_wildcard_names(self):
+		return set(match.group('name') for match in re.finditer(self.wildcard_regex, self._file))
+	
+	def regex(self):
+		f = re.sub("\.", "\.", self._file)
+		return re.sub(self.wildcard_regex, 
+			lambda match: '(?P<{}>{})'.format(match.group('name'), match.group('constraint') if match.group('constraint') else ".+"), f)
+	
+	def __str__(self):
+		return self._file
+
+def temp(file):
+	return IOFile.create(file, temp = True)
+
+def protected(file):
+	return IOFile.create(file, protected = True)
