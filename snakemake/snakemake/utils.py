6dd184fe76cde12e58cd985f33492713237ea2a3
johannes.koester@gmail.com <johannes.koester@gmail.com@9dc630ac-28ba-dcf0-b748-d416c9740215>
2012-08-13 13:18:12 +0000

    Minor.

--- snakemake/snakemake/utils.py
+++ snakemake/snakemake/utils.py
@@ -6,10 +6,24 @@ from snakemake.io import regex, Namedlist
 __author__ = "Johannes Köster"
 
 def linecount(filename):
+	"""
+	Return the number of lines of given file
+	
+	Arguments
+	filename -- the path to the file
+	"""
 	with open(filename) as f:
 		return sum(1 for l in f)
 
 def listdir(dirname, pattern = None):
+	"""
+	Yield a tuple of filenames and full filepaths in the given directory.
+	If pattern is specified, wildcard values are yielded as the third tuple item.
+
+	Arguments
+	dirname -- the path to the directory to list
+	pattern -- an optional filepattern relative to dirname. Wildcards are specified in snakemake syntax, e.g. "{id}.txt"
+	"""
 	if pattern:
 		pattern = re.compile(regex(pattern))
 		for f in os.listdir(dirname):
@@ -24,6 +38,9 @@ def listdir(dirname, pattern = None):
 			yield f, filepath
 
 def makedirs(dirnames):
+	"""
+	Recursively create the given directory or directories without reporting errors if they are present.
+	"""
 	if isinstance(dirnames, str):
 		dirnames = [dirnames]
 	for dirname in dirnames:
