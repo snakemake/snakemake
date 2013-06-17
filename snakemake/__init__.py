1306188c4e9d6b65127388a92726e417480ed4f9
Johannes Köster <johannes.koester@tu-dortmund.de>
2013-06-17 10:49:27 +0200

    New upstream release prepared.

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -7,7 +7,7 @@ from snakemake.exceptions import print_exception
 from snakemake.logging import logger, init_logger
 
 __author__ = "Johannes Köster"
-__version__ = "2.3"
+__version__ = "2.4"
 
 
 def snakemake(snakefile,
