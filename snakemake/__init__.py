5ff80600eadc2975f28515b2fcb031bc70ec3f70
Johannes Köster <johannes.koester@tu-dortmund.de>
2013-10-13 19:24:58 +0200

    Version bump

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -16,7 +16,7 @@ from snakemake.exceptions import print_exception
 from snakemake.logging import logger, init_logger
 
 __author__ = "Johannes Köster"
-__version__ = "2.4.6"
+__version__ = "2.4.7"
 
 
 def snakemake(snakefile,
