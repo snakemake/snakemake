bc57284a61236ebee032c0c1bd74882fa1e54af4
Johannes Köster <johannes.koester@tu-dortmund.de>
2014-02-01 17:50:25 +0100

    Minor.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
