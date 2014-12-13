0a859d0585e875444da35173a352336be8138e29
Kemal Eren <kemal@kemaleren.com>
2014-12-13 01:52:53 -0800

    Fix a critical test bug.
    `test.run()` was looking in the wrong place for expected files, then
    skipping the actual checks.

--- tests/tests.py
+++ tests/tests.py
@@ -52,7 +52,8 @@ def run(path, shouldfail=False, needs_connection=False, snakefile="Snakefile", *
         else:
             assert success, "expected successful execution"
             for resultfile in os.listdir(results_dir):
-                if not os.path.isfile(resultfile):
+                if not os.path.isfile(os.path.join(results_dir, resultfile)):
+                    # FIXME: this means tests cannot use directories
                     continue # skip .svn dirs etc.
                 targetfile = join(tmpdir, resultfile)
                 expectedfile = join(results_dir, resultfile)
