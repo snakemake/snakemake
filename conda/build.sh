2cf99238bddae85f9561f613264ebd1e00495381
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-05-19 11:30:16 -0400

    TODO.

--- conda/build.sh
+++ conda/build.sh
@@ -2,4 +2,11 @@
 
 conda build snakemake
 conda convert ~/miniconda3/conda-bld/linux-64/snakemake-*.tar.bz2 -p all
-binstar upload --force */snakemake-*.tar.bz2
+echo Due to a bug in conda convert, the subdir in info/index.json is not updated (to e.g. win-64).
+echo This has to be done manually in the tarball.
+exit 0
+# TODO reactivate this once conda convert has been fixed.
+for p in */snakemake-*.tar.bz2
+do
+    binstar upload $p
+done
