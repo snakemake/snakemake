1bb6ffa33d054d881ce7b81a83f85fd8fc4bb7e3
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-09-23 13:22:36 -0400

    Removed custom memoizer at the suggestion of Johannes Köster, who pointed out LRU-Cache exists.

--- snakemake/decorators.py
+++ snakemake/decorators.py
@@ -6,20 +6,6 @@ __license__ = "MIT"
 import functools
 import inspect
 
-
-def memoize(obj):
-    cache = obj.cache = {}
-
-    @functools.wraps(obj)
-    def memoizer(*args, **kwargs):
-        key = str(args) + str(kwargs)
-        if key not in cache:
-            cache[key] = obj(*args, **kwargs)
-        return cache[key]
-
-    return memoizer
-
-
 def decAllMethods(decorator, prefix='test_'):
 
     def decClass(cls):
