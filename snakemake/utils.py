21a5483d4cb2e9afd306810ec64f97e748e8feb9
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-07 20:16:43 +0200

    Add newlines and use set

--- snakemake/utils.py
+++ snakemake/utils.py
@@ -247,11 +247,12 @@ def set_temporary_output(*rules):
     """Set the output of rules to temporary"""
     for rule in rules:
         logger.debug("setting output of rule '{rule}' to temporary".format(rule=rule))
-        rule.temp_output = rule.output
+        rule.temp_output = set(rule.output)
+
 
 def set_protected_output(*rules):
     """Set the output of rules to protected"""
     for rule in rules:
         logger.debug("setting output of rule '{rule}' to protected".format(rule=rule))
-        rule.protected_output = rule.output
+        rule.protected_output = set(rule.output)
 
