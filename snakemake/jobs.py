600af450ba3b15ea3a2d56df98c68c06b09fe21b
Rasmus Ågren <rasmus.agren@scilifelab.se>
2016-10-18 23:46:21 +0200

    -

--- snakemake/jobs.py
+++ snakemake/jobs.py
@@ -425,6 +425,8 @@ class Job:
             print("end output\n")
             print(self.log)
             print("end log\n")
+            print(self.params)
+            print("end log\n")
             for f in set([item for sublist in [self.input,self.output,self.log,self.params] if sublist is not None for item in sublist]):
                 print(f)
                 # Only link to relative paths below cwd
