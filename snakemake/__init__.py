42842f3288641f58af519ae5bc3c5ccbe84cb724
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-06-14 19:06:23 +0200

    Enable SFTP as default remote, and add choices to the command line argument.

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -1022,6 +1022,7 @@ def get_argument_parser():
         "a different URL to use your fork or a local clone of the repository."
     )
     parser.add_argument("--default-remote-provider",
+                        choices=["S3", "GS", "SFTP", "S3Mocked"],
                         help="Specify default remote provider to be used for "
                         "all input and output files that don't yet specify "
                         "one.")
