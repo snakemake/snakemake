c03fbe3802637ac363bb909a35910551d674e23b
Oliver Stolpe <oliver.stolpe@bihealth.de>
2017-08-22 13:27:17 +0200

    added irods remote functionality + docs

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -1234,7 +1234,7 @@ def get_argument_parser(profile=None):
         "a different URL to use your fork or a local clone of the repository."
     )
     parser.add_argument("--default-remote-provider",
-                        choices=["S3", "GS", "FTP", "SFTP", "S3Mocked", "gfal", "gridftp"],
+                        choices=["S3", "GS", "FTP", "SFTP", "S3Mocked", "gfal", "gridftp", "iRODS"],
                         help="Specify default remote provider to be used for "
                         "all input and output files that don't yet specify "
                         "one.")
