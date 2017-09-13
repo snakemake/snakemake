6a467032198ac8e869a225cf81f886e73c674adc
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-09-13 14:52:38 +0200

    Cleanup metadata on host if not assuming shared fs.

--- snakemake/executors.py
+++ snakemake/executors.py
@@ -567,6 +567,9 @@ class ClusterExecutor(RealExecutor):
         # TODO what about removing empty remote dirs?? This cannot be decided
         # on the cluster node.
         super().handle_job_error(job, upload_remote=False)
+        if not self.assume_shared_fs:
+            # if no shared fs, we have to remove metadata here as well
+            self.workflow.persistence.cleanup(job)
 
 
 GenericClusterJob = namedtuple("GenericClusterJob", "job jobid callback error_callback jobscript jobfinished jobfailed")
