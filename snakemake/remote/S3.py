e14952276dc46e1f617dc4e595f4ee0744dff770
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-09-27 20:48:00 -0400

    added automatic decompression of s3 objects on download where content encoding is set to gzip. can be overridden via streaming_decompress=True in the remote object args

--- snakemake/remote/S3.py
+++ snakemake/remote/S3.py
@@ -8,11 +8,20 @@ import os
 import re
 import math
 import functools
-import concurrent.futures
+from concurrent import futures
+import contextlib
+from io import BytesIO
+import itertools
+import tempfile
+import gzip
+import zlib
+import shutil
+from functools import partial
 
 # module-specific
 from snakemake.remote import AbstractRemoteObject, AbstractRemoteProvider
 from snakemake.exceptions import WorkflowError, S3FileException
+from snakemake.utils import available_cpu_count
 
 try:
     # third-party modules
@@ -22,6 +31,38 @@ except ImportError as e:
     raise WorkflowError("The Python 3 package 'boto3' "
         "needs to be installed to use S3 remote() file functionality. %s" % e.msg)
 
+
+def range_finder(length, range_size):
+    if range_size>length:
+        return (0,length)
+
+    return [(lambda x: (x,(x+range_size)-1) if x+range_size<=length else (x,length))(x) for x in range(0,length+1)[::range_size]]
+
+def grouper(src, size):
+    src_iter = iter(src)
+    while True:
+        cur_chunk = list(itertools.islice(src_iter, size))
+        if not cur_chunk:
+            break
+        yield cur_chunk
+
+def grouped_ranges(length, chunk_size, chunks_per_group):
+    return grouper(range_finder(length, chunk_size), chunks_per_group)
+
+
+def ranged_get(bucket_name, key, kwargs, range):
+    """
+    Params:
+        k: boto3 object
+        range: two-tuple of the byte range to return (inclusive)
+    """
+    boto3_session = boto3.session.Session()
+    s3 = boto3_session.resource('s3', **kwargs)
+    obj = s3.Object(bucket_name, key)
+
+    range_str = "bytes={}-{}".format(*range)
+    res = obj.get(Range=range_str)["Body"].read()
+    return res
 
 class RemoteProvider(AbstractRemoteProvider):
 
@@ -52,7 +93,6 @@ class RemoteObject(AbstractRemoteObject):
 
     def __init__(self, *args, keep_local=False, provider=None, **kwargs):
         super(RemoteObject, self).__init__(*args, keep_local=keep_local, provider=provider, **kwargs)
-
         if provider:
             self._s3c = provider.remote_interface()
         else:
@@ -79,7 +119,7 @@ class RemoteObject(AbstractRemoteObject):
             return self._iofile.size_local
 
     def download(self):
-        self._s3c.download_from_s3(self.s3_bucket, self.s3_key, self.local_file())
+        self._s3c.download_from_s3(self.s3_bucket, self.s3_key, self.local_file(), auto_decompress=self.kwargs.get("auto_decompress", True), streaming_decompress=self.kwargs.get("streaming_decompress", False))
         os.sync() # ensure flush to disk
 
     def upload(self):
@@ -129,6 +169,7 @@ class S3Helper(object):
 
         # allow key_id and secret to be specified with aws_, gs_, or no prefix.
         # Standardize to the aws_ prefix expected by boto.
+
         if "gs_access_key_id" in kwargs:
             kwargs["aws_access_key_id"] = kwargs.pop("gs_access_key_id")
         if "gs_secret_access_key" in kwargs:
@@ -137,6 +178,8 @@ class S3Helper(object):
             kwargs["aws_access_key_id"] = kwargs.pop("access_key_id")
         if "secret_access_key" in kwargs:
             kwargs["aws_secret_access_key"] = kwargs.pop("secret_access_key")
+
+        self.init_kwargs = kwargs
 
         self.s3 = boto3.resource('s3', **kwargs)
 
@@ -155,6 +198,7 @@ class S3Helper(object):
             use_relative_path_for_key=True,
             relative_start_dir=None,
             extra_args=None,
+            gzip_compress=False,
             config=None):
         """ Upload a file to S3
 
@@ -194,7 +238,20 @@ class S3Helper(object):
         k = self.s3.Object(bucket_name, key)
 
         try:
-            k.upload_file(file_path, ExtraArgs=extra_args, Config=config)
+            if gzip_compress:
+                if not extra_args:
+                    extra_args = {'ContentEncoding': 'gzip'}
+                else:
+                    extra_args['ContentEncoding'] = 'gzip'
+
+                if not compressed_fp:
+                    compressed_fp = BytesIO()
+                with gzip.GzipFile(fileobj=compressed_fp, mode='wb') as gz:
+                    shutil.copyfileobj(file_path, gz)
+                compressed_fp.seek(0)
+                k.upload_fileobj(compressed_fp, ExtraArgs=extra_args, Config=config)
+            else:
+                k.upload_file(file_path, ExtraArgs=extra_args, Config=config)
         except:
             raise
             return None
@@ -206,7 +263,9 @@ class S3Helper(object):
             destination_path=None,
             expandKeyIntoDirs=True,
             make_dest_dirs=True,
-            create_stub_only=False):
+            create_stub_only=False,
+            auto_decompress=True,
+            streaming_decompress=False):
         """ Download a file from s3
 
             This function downloads an object from a specified AWS S3 bucket.
@@ -243,10 +302,67 @@ class S3Helper(object):
             os.makedirs(os.path.dirname(destination_path), exist_ok=True)
 
         k = self.s3.Object(bucket_name, key)
-
         try:
             if not create_stub_only:
-                k.download_file(destination_path)
+                if auto_decompress:
+                    if k.content_encoding == "gzip":  
+                        if not streaming_decompress:                       
+                            def streamingbody_iter(streamingbody, chunk_size=1024):
+                                '''
+                                    Construct a generator to return reasonably large data chunks
+                                    from boto3's StreamingBody object, which by default
+                                    returns everything (loading it into mem).
+                                '''
+                                chunk = streamingbody.read(amt=chunk_size)
+                                while chunk[0] != b"": #bytearray():
+                                    yield chunk
+                                    chunk = streamingbody.read(amt=chunk_size)
+
+                            obj_ranged_get = partial(ranged_get, bucket_name, key, self.init_kwargs)
+
+                            with tempfile.TemporaryFile() as temp_out:
+                                count = 0
+                                decompressor = zlib.decompressobj(32 + zlib.MAX_WBITS)  # offset 32 to skip the header
+                                with futures.ProcessPoolExecutor(max_workers=available_cpu_count()*2) as executor:
+                                    for range_group in grouped_ranges(k.content_length, 20*1024*1024, int(available_cpu_count()*1.25)):
+                                        for res in executor.map(obj_ranged_get, range_group, chunksize=1):
+                                            decompressed_chunk = decompressor.decompress(res)
+                                            count+=1
+                                            if decompressed_chunk:
+                                                temp_out.write(decompressed_chunk)
+                                        os.sync()
+                                        temp_out.flush()
+                                shutil.move(temp_out, destination_path)                        
+                        else:
+                            # streaming decompression without runaway memory usage
+                            # slower than chunked downloading
+                            def stream_gzip_decompress(stream):
+                                decompressor = zlib.decompressobj(32 + zlib.MAX_WBITS)  # offset 32 to skip the header
+                                for chunk in stream:
+                                    decompressed_chunk = decompressor.decompress(chunk)
+                                    if decompressed_chunk:
+                                        yield decompressed_chunk
+
+                            def streamingbody_iter(streamingbody, chunk_size=20*1024*1024):
+                                '''
+                                    Construct a generator to return reasonably large data chunks
+                                    from boto3's StreamingBody object, which by default
+                                    returns everything (loading it into mem).
+                                '''
+                                chunk = streamingbody.read(amt=chunk_size)
+                                while chunk[0] != bytearray():
+                                    yield chunk
+                                    chunk = streamingbody.read(amt=chunk_size)
+
+                            fd,temp_out = tempfile.mkstemp()
+                            count = 0
+                            with open(temp_out,"wb") as outf:
+                                for data in stream_gzip_decompress( streamingbody_iter(k.get()["Body"]) ):
+                                    outf.write(data)
+                                    outf.flush()
+                            shutil.move(temp_out, destination_path)
+                else:
+                    k.download_file(destination_path)
             else:
                 # just create an empty file with the right timestamps
                 with open(destination_path, 'wb') as fp:
