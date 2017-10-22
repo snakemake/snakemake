b9a7bf6a8a7e16c77b1da05280078191e8836b80
bow <bow@bow.web.id>
2017-10-22 19:02:20 +0200

    Fix default singularity_args value to empty string instead of `None`

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -1214,6 +1214,7 @@ def get_argument_parser(profile=None):
         "invocation directory, or as an absolute path.")
     parser.add_argument(
         "--singularity-args",
+        default="",
         metavar="ARGS",
         help="Pass additional args to singularity.")
     parser.add_argument(
