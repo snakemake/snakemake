574df02165a31f4f02b2619e39a9a9e6f506e1d2
Oliver Stolpe <oliver.stolpe@bihealth.de>
2017-12-18 19:22:09 +0100

    using now single session that is passed, credentials via config file

--- snakemake/remote/iRODS.py
+++ snakemake/remote/iRODS.py
@@ -4,12 +4,14 @@ __email__ = "oliver.stolpe@bihealth.org"
 __license__ = "MIT"
 
 import os
+import re
+
 from contextlib import contextmanager
 from datetime import datetime, timedelta
 from pytz import timezone
 
 # module-specific
-from snakemake.remote import AbstractRemoteProvider, DomainObject
+from snakemake.remote import AbstractRemoteProvider, AbstractRemoteObject
 from snakemake.exceptions import WorkflowError
 
 try:
@@ -17,6 +19,7 @@ try:
     from irods.session import iRODSSession
     from irods.meta import iRODSMeta
     from irods.models import DataObject
+    from irods.exception import CollectionDoesNotExist
     import irods.keywords as kw
 except ImportError as e:
     raise WorkflowError("The Python 3 package 'python-irodsclient' " +
@@ -26,90 +29,70 @@ except ImportError as e:
 utc = datetime.utcfromtimestamp(0)
 
 
+def _irods_session(*args, **kwargs):
+    try:
+        irods_env_file = os.environ['IRODS_ENVIRONMENT_FILE']
+    except KeyError:
+        irods_env_file = kwargs.get('env', os.path.expanduser('~/.irods/irods_environment.json'))
+
+    if not os.path.isfile(irods_env_file):
+        raise WorkflowError("Expecting %s but none found." % irods_env_file)
+    
+    # 1. add the .irodsA password file  via irods_environment.json, e.g.
+    #    "irods_authentication_file": "path-to-file/.irodsA"
+    # 2. have it available at ~/.irods/.irodsA
+    # 3. define environment variable IRODS_ENVIRONMENT_FILE, e.g.
+    #    export IRODS_ENVIRONMENT_FILE=path-to-file/.irodsA
+
+    with iRODSSession(irods_env_file=irods_env_file) as session:
+        try:
+            session.collections.get(os.path.join(os.sep, session.zone, 'home', session.username))
+        except ConnectionError as e:
+            raise e
+
+        return session
+
+
 class RemoteProvider(AbstractRemoteProvider):
 
     supports_default = True
 
     def __init__(self, *args, stay_on_remote=False, **kwargs):
         super(RemoteProvider, self).__init__(*args, stay_on_remote=stay_on_remote, **kwargs)
+        self._irods_session = _irods_session(*args, **kwargs)
+
+    def remote_interface(self):
+        return self._irods_session
 
     @property
     def default_protocol(self):
         """The protocol that is prepended to the path when no protocol is specified."""
-        return ''
+        return 'irods://'
 
     @property
     def available_protocols(self):
         """List of valid protocols for this remote provider."""
-        return []
+        return ['irods://']
 
 
-class RemoteObject(DomainObject):
+class RemoteObject(AbstractRemoteObject):
     """ This is a class to interact with an iRODS server.
     """
 
     def __init__(self, *args, keep_local=False, provider=None, **kwargs):
         super(RemoteObject, self).__init__(*args, keep_local=keep_local, provider=provider, **kwargs)
-
-    # === Implementations of abstract class members ===
-
-    @contextmanager #makes this a context manager. after 'yield' is __exit__()
-    def irods_session(self):
-        # if args have been provided to remote(), use them over those given to RemoteProvider()
-        args_to_use = self.provider.args
-        if len(self.args):
-            args_to_use = self.args
-
-        zone = self.remote_path.split(os.sep)[1]
-
-        # use kwargs passed in to remote() to override those given to the RemoteProvider()
-        # default to the host and port given as part of the file, falling back to one specified
-        # as a kwarg to remote() or the RemoteProvider (overriding the latter with the former if both)
-        irods_env_file = os.path.expanduser('~/.irods/irods_environment.json')
-        irods_passwd_file = os.path.expanduser('~/.irods/.irodsA')
-
-        if not 'user' in self.provider.kwargs:
-            #print("%s: '%s' as host is ignored, using host from %s" % (self.local_file(), self.host, irods_env_file))
-            try:
-                irods_env_file = os.environ['IRODS_ENVIRONMENT_FILE']
-            except:
-                if not os.path.isfile(irods_env_file):
-                    raise WorkflowError("Expecting %s but none found" % irods_env_file)
-
-            if not os.path.isfile(irods_passwd_file):
-                raise WorkflowError("Expecting %s but none found. Did you perfom iinit?" % irods_passwd_file)\
-
-            session = iRODSSession(irods_env_file=irods_env_file)
+        if provider:
+            self._irods_session = provider.remote_interface()
+            self._timezone = provider.kwargs.get('timezone', None)
         else:
-            kwargs_to_use = {}
-            kwargs_to_use["host"] = self.host
-            kwargs_to_use["port"] = int(self.port) if self.port else 1247
-            kwargs_to_use["zone"] = self.provider.kwargs.get('zone', zone)
-            kwargs_to_use["user"] = self.provider.kwargs['user']
-            kwargs_to_use["password"] = self.provider.kwargs['password']
-
-            # do no take everything from provider.kwargs because then it also passes the timezone
-            for k,v in self.kwargs.items():
-                # allow only for the previously defined parameters
-                # otherwise the 'overwrite' parameter would also be passed
-                if k not in kwargs_to_use.keys():
-                    continue
-                kwargs_to_use[k] = v
-
-            session = iRODSSession(**kwargs_to_use)
-        yield session
-        session.cleanup()
+            self._irods_session = _irods_session(*args, **kwargs)
 
     def exists(self):
-        if self._matched_address:
-            with self.irods_session() as session:
-                try:
-                    obj = session.data_objects.get(self.remote_path)
-                    return True
-                except:
-                    return False
-        else:
-            raise WorkflowError("The file cannot be parsed as an iRODS path in form 'host:port/path/to/file': %s" % self.local_file())
+        try:
+            self._irods_session.data_objects.get(self.remote_path)
+            return True
+        except CollectionDoesNotExist:
+            return False
 
     def _convert_time(self, timestamp, tz=None):
         dt = timestamp.replace(tzinfo=timezone('UTC'))
@@ -119,45 +102,43 @@ class RemoteObject(DomainObject):
 
     def mtime(self):
         if self.exists():
-            with self.irods_session() as session:
-                obj = session.data_objects.get(self.remote_path)
-                meta = session.metadata.get(DataObject, self.remote_path)
+            obj = self._irods_session.data_objects.get(self.remote_path)
+            meta = self._irods_session.metadata.get(DataObject, self.remote_path)
 
-                # if mtime was set in metadata during upload, take this information
-                # otherwise fall back to iRODS timestamp (upload time!) and change
-                # timezone accordingly (iRODS might ignore the servers local timezone)
-                for m in meta:
-                    if m.name == 'mtime':
-                        mtime = float(m.value)
-                        break
-                else:
-                    dt = self._convert_time(obj.modify_time, self.provider.kwargs.get('timezone', None))
-                    utc2 = self._convert_time(utc)
-                    mtime = (dt - utc2).total_seconds()
-                
-                return int(mtime)
+            # if mtime was set in metadata during upload, take this information
+            # otherwise fall back to iRODS timestamp (upload time!) and change
+            # timezone accordingly (iRODS might ignore the servers local timezone)
+            for m in meta:
+                if m.name == 'mtime':
+                    mtime = float(m.value)
+                    break
+            else:
+                dt = self._convert_time(obj.modify_time, self._timezone)
+                utc2 = self._convert_time(utc)
+                mtime = (dt - utc2).total_seconds()
+            
+            return int(mtime)
         else:
             raise WorkflowError("The file does not seem to exist remotely: %s" % self.local_file())
 
     def atime(self):
         if self.exists():
-            with self.irods_session() as session:
-                obj = session.data_objects.get(self.remote_path)
-                meta = session.metadata.get(DataObject, self.remote_path)
+            obj = self._irods_session.data_objects.get(self.remote_path)
+            meta = self._irods_session.metadata.get(DataObject, self.remote_path)
 
-                # if mtime was set in metadata during upload, take this information
-                # otherwise fall back to iRODS timestamp (upload time!) and change
-                # timezone accordingly (iRODS might ignore the servers local timezone)
-                for m in meta:
-                    if m.name == 'atime':
-                        atime = float(m.value)
-                        break
-                else:
-                    dt = self._convert_time(obj.modify_time, self.provider.kwargs.get('timezone', None))
-                    utc2 = self._convert_time(utc)
-                    atime = (dt - utc2).total_seconds()
+            # if mtime was set in metadata during upload, take this information
+            # otherwise fall back to iRODS timestamp (upload time!) and change
+            # timezone accordingly (iRODS might ignore the servers local timezone)
+            for m in meta:
+                if m.name == 'atime':
+                    atime = float(m.value)
+                    break
+            else:
+                dt = self._convert_time(obj.modify_time, self._timezone)
+                utc2 = self._convert_time(utc)
+                atime = (dt - utc2).total_seconds()
 
-                return int(atime)
+            return int(atime)
         else:
             raise WorkflowError("File doesn't exist remotely: %s" % self.local_file())
 
@@ -168,71 +149,79 @@ class RemoteObject(DomainObject):
         
     def size(self):
         if self.exists():
-            with self.irods_session() as session:
-                obj = session.data_objects.get(self.remote_path)
-                return int(obj.size)
+            obj = self._irods_session.data_objects.get(self.remote_path)
+            return int(obj.size)
         else:
             return self._iofile.size_local
 
     def download(self, make_dest_dirs=True):
-        with self.irods_session() as session:
-            if self.exists():
-                if make_dest_dirs:
-                    os.makedirs(os.path.dirname(self.local_path), exist_ok=True)
+        if self.exists():
+            if make_dest_dirs:
+                os.makedirs(os.path.dirname(self.local_path), exist_ok=True)
 
-                # force irods to overwrite existing file if this option is set
-                opt = {}
-                if self.kwargs.get('overwrite'):
-                    opt[kw.FORCE_FLAG_KW] = ''
+            # force irods to overwrite existing file if this option is set
+            opt = {}
+            if self.kwargs.get('overwrite'):
+                opt[kw.FORCE_FLAG_KW] = ''
 
-                # get object and change timestamp
-                obj = session.data_objects.get(self.remote_path, self.local_path, options=opt)
-                os.utime(self.local_path, (self.atime(), self.mtime()))
-                os.sync()
-            else:
-                raise WorkflowError("The file does not seem to exist remotely: %s" % self.local_file())
+            # get object and change timestamp
+            obj = self._irods_session.data_objects.get(self.remote_path, self.local_path, options=opt)
+            os.utime(self.local_path, (self.atime(), self.mtime()))
+            os.sync()
+        else:
+            raise WorkflowError("The file does not seem to exist remotely: %s" % self.local_file())
 
     def upload(self):
-        with self.irods_session() as session:
-            # get current local timestamp
-            stat = os.stat(self.local_path)
+        # get current local timestamp
+        stat = os.stat(self.local_path)
 
-            # create folder structure on remote
-            folders = os.path.dirname(self.remote_path).split(os.sep)[1:]
-            collpath = os.sep
+        # create folder structure on remote
+        folders = os.path.dirname(self.remote_path).split(os.sep)[1:]
+        collpath = os.sep
 
-            for folder in folders:
-                collpath = os.path.join(collpath, folder)
+        for folder in folders:
+            collpath = os.path.join(collpath, folder)
 
-                try:
-                    session.collections.get(collpath)
-                except:
-                    session.collections.create(collpath)
+            try:
+                self._irods_session.collections.get(collpath)
+            except:
+                self._irods_session.collections.create(collpath)
 
-            # upload file and store local timestamp in metadata since irods sets the files modification time to
-            # the upload time rather than retaining the local modification time
-            session.data_objects.put(self.local_path, self.remote_path)
+        # upload file and store local timestamp in metadata since irods sets the files modification time to
+        # the upload time rather than retaining the local modification time
+        self._irods_session.data_objects.put(self.local_path, self.remote_path)
 
-            # erase meta data (if exists) before adding it. there is no update routine available in the API
-            for m in session.metadata.get(DataObject, self.remote_path):
-                if m.name in ('mtime', 'atime', 'ctime'):
-                    session.metadata.remove(DataObject, self.remote_path, iRODSMeta(m.name, m.value, m.units))
+        # erase meta data (if exists) before adding it. there is no update routine available in the API
+        for m in self._irods_session.metadata.get(DataObject, self.remote_path):
+            if m.name in ('mtime', 'atime', 'ctime'):
+                self._irods_session.metadata.remove(DataObject, self.remote_path, iRODSMeta(m.name, m.value, m.units))
 
-            session.metadata.add(DataObject, self.remote_path, iRODSMeta('mtime', str(stat.st_mtime), 's'))
-            session.metadata.add(DataObject, self.remote_path, iRODSMeta('atime', str(stat.st_atime), 's'))
-            session.metadata.add(DataObject, self.remote_path, iRODSMeta('ctime', str(stat.st_ctime), 's'))
+        self._irods_session.metadata.add(DataObject, self.remote_path, iRODSMeta('mtime', str(stat.st_mtime), 's'))
+        self._irods_session.metadata.add(DataObject, self.remote_path, iRODSMeta('atime', str(stat.st_atime), 's'))
+        self._irods_session.metadata.add(DataObject, self.remote_path, iRODSMeta('ctime', str(stat.st_ctime), 's'))
+
+    @property
+    def remote_path(self):
+        return os.path.join(os.sep, self._irods_session.zone, self.local_file())
+
+    @property
+    def name(self):
+        return self.local_file()
+
+    @property
+    def local_path(self):
+        return self.local_file()
 
     @property
     def list(self):
         file_list = []
 
         first_wildcard = self._iofile.constant_prefix()
-        dirname = os.path.dirname(first_wildcard.replace(self.path_prefix, ""))
+        dirname = os.path.dirname(first_wildcard)
 
-        with self.irods_session() as session:
-            collection = session.collections.get(dirname)
-            for current_collection, subcollections, objs in collection.walk():
-                for obj in objs:
-                    file_list.append(obj.path.lstrip('/'))
+        collection = self._irods_session.collections.get(dirname)
+        for _, _, objs in collection.walk():
+            for obj in objs:
+                file_list.append(obj.path.lstrip('/'))
 
         return file_list
