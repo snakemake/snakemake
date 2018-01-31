c399eda122c1f3560cc8695f0c854bc0f4c411d8
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-01-31 16:18:25 +0100

    Better error message if a file disappears in is_newer

--- snakemake/io.py
+++ snakemake/io.py
@@ -258,7 +258,10 @@ class _IOFile(str):
             #is the best we can do.
             return self.mtime > time
         else:
-            return os.stat(self, follow_symlinks=True).st_mtime > time or self.mtime > time
+            try:
+                return os.stat(self, follow_symlinks=True).st_mtime > time or self.mtime > time
+            except FileNotFoundError:
+                raise WorkflowError("File {} not found although it existed before. Is there another active process that might have deleted it?")
 
     def download_from_remote(self):
         if self.is_remote and self.remote_object.exists():
