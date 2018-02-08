eb436b9979956fb2306b05b082480caaf34d3878
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-02-08 08:23:18 +0100

    Cleanup

--- snakemake/dag.py
+++ snakemake/dag.py
@@ -373,8 +373,9 @@ class DAG:
         """ Move files from shadow directory to real output paths. """
         if not job.shadow_dir or not job.expanded_output:
             return
+
         files = job.log if only_log else chain(job.expanded_output, job.log)
-        print(files)
+
         for real_output in files:
             shadow_output = job.shadowed_path(real_output).file
             # Remake absolute symlinks as relative
