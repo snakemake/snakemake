94706b2971f69a841faabf60448c1249fc2ad7ef
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-02-16 13:23:18 +0100

    Refactoring.

---
+++ .circleci/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+if type conda > /dev/null; then exit 0; fi
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p miniconda
+conda env create --name snakemake --file test-environment.yml
