ff4e13e2288d61a5f13621b6df2d2493affa3f8d
Johannes Köster <johannes.koester@uni-due.de>
2019-10-05 23:26:04 +0200

    move apt install out of cache

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -3,18 +3,14 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-# conda
+# setup conda
 if type conda > /dev/null; then exit 0; fi
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
 conda env create --quiet --name snakemake --file test-environment.yml
 conda create --quiet -n black black
 
-# stress
-sudo apt-get update
-sudo apt-get install stress
-
-# singularity
+# install singularity
 source .circleci/common.sh
 sudo add-apt-repository -y ppa:gophers/archive
 sudo apt-get update
