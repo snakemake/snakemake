bcf7d4f8bca373f59559abbddca19c1b9ed9305c
Johannes Köster <johannes.koester@uni-due.de>
2019-10-05 22:57:32 +0200

    install black

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -8,6 +8,7 @@ if type conda > /dev/null; then exit 0; fi
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
 conda env create --name snakemake --file test-environment.yml
+conda create -n black black
 
 # stress
 sudo apt-get update
