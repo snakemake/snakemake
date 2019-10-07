410798acc3b66885cd37d12a179b13204e9ebd2c
Johannes Köster <johannes.koester@uni-due.de>
2019-10-07 16:58:31 +0200

    Fix protected logic

--- snakemake/io.py
+++ snakemake/io.py
@@ -282,7 +282,7 @@ class _IOFile(str):
     @property
     def protected(self):
         # symlinks are never regarded as protected
-        return self.exists_local and os.access(
+        return self.exists_local and not os.access(
             self.file,
             os.W_OK,
             follow_symlinks=os.access not in os.supports_follow_symlinks,
