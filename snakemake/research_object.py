a3cfc506b7e85acd51aa1b634813513a451dbe9a
Johannes Köster <johannes.koester@uni-due.de>
2019-10-07 17:39:13 +0200

    licensing

--- snakemake/research_object.py
+++ snakemake/research_object.py
@@ -1,3 +1,11 @@
+__author__ = "Alban Gaignard, Johannes Köster"
+__copyright__ = "Copyright 2019, Alban Gaignard, Johannes Köster"
+__license__ = "MIT"
+
+import itertools
+
+from snakemake.io import is_flagged
+
 def create_research_object(dag, path):
     """Create a research object from workflow persistence information."""
 
