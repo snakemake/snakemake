924abccb31098ec336980f78a9d43343e1eaa676
Johannes Köster <johannes.koester@uni-due.de>
2019-10-14 11:21:24 +0200

    Fix keyword args for azure storage and S3. (#31)

--- snakemake/remote/S3.py
+++ snakemake/remote/S3.py
@@ -32,10 +32,10 @@ class RemoteProvider(
     supports_default = True  # class variable
 
     def __init__(
-        self, *args, stay_on_remote=False, **kwargs
-    ):  # this method is evaluated when instantiating this class
+        self, *args, keep_local=False, stay_on_remote=False, is_default=False, **kwargs
+    ):
         super(RemoteProvider, self).__init__(
-            *args, stay_on_remote=stay_on_remote, **kwargs
+            *args, keep_local=keep_local, stay_on_remote=stay_on_remote, is_default=is_default, **kwargs
         )  # in addition to methods provided by AbstractRemoteProvider, we add these in
 
         self._s3c = S3Helper(*args, **kwargs)  # _private variable by convention
