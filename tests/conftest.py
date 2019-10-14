cecc022bfa666f6f6d2175a40cf94b8a2f54fdd6
Morten Enemark Lund <melund@gmail.com>
2019-10-14 14:45:46 +0200

    Reset sys.path between tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,15 @@
+import sys
 import pytest
 
 from snakemake.utils import ON_WINDOWS
 
 skip_on_windows = pytest.mark.skipif(ON_WINDOWS, reason="Unix stuff")
+
+
+@pytest.fixture(autouse=True)
+def reset_paths_between_tests():
+    """Ensure that changes to sys.path are reset between tests"""
+    org_path = sys.path.copy()
+    yield
+    sys.path = org_path
+
