8d1495775c0d77aa37195b2709aa25a3605e1392
Johannes Köster <johannes.koester@uni-due.de>
2019-10-16 11:58:58 +0200

    Show failed logs. Formatting (#37)

--- snakemake/remote/S3.py
+++ snakemake/remote/S3.py
@@ -35,7 +35,11 @@ class RemoteProvider(
         self, *args, keep_local=False, stay_on_remote=False, is_default=False, **kwargs
     ):
         super(RemoteProvider, self).__init__(
-            *args, keep_local=keep_local, stay_on_remote=stay_on_remote, is_default=is_default, **kwargs
+            *args,
+            keep_local=keep_local,
+            stay_on_remote=stay_on_remote,
+            is_default=is_default,
+            **kwargs
         )  # in addition to methods provided by AbstractRemoteProvider, we add these in
 
         self._s3c = S3Helper(*args, **kwargs)  # _private variable by convention
