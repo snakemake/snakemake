ff95e5f45e0ade06b04b9084ae7ea493ee9af30a
Johannes Köster <johannes.koester@uni-due.de>
2019-10-22 16:36:05 +0200

    Use snakemake shell function

--- tests/tests.py
+++ tests/tests.py
@@ -924,13 +924,11 @@ def test_issue635():
 def test_convert_to_cwl():
     workdir = dpath("test_convert_to_cwl")
     # run(workdir, export_cwl=os.path.join(workdir, "workflow.cwl"))
-    subprocess.check_call(
-        "python -m snakemake --export-cwl workflow.cwl",
-        cwd=workdir,
-        env={"PYTHONPATH": os.getcwd()},
-        shell=True,
+    shell(
+        "cd {workdir}; PYTHONPATH={src} python -m snakemake --export-cwl workflow.cwl",
+        src=os.getcwd(),
     )
-    subprocess.check_call("cwltool --singularity workflow.cwl", cwd=workdir, shell=True)
+    shell("cd {workdir}; cwltool --singularity workflow.cwl")
     assert os.path.exists(os.path.join(workdir, "test.out"))
 
 
