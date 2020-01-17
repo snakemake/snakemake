47cc6a9dab9cc75946da8a7aaffe6efb07a67bf2
Troy Comi <troycomi@gmail.com>
2020-01-17 08:27:04 -0500

    Add allow_missing in expand (#127)
    * Add allow_missing in expand
    
    Added an optional keyword argument to expand which will override the
    default behavior when wildcards in the filepath are not present.  If
    allow_missing=True and it is not used in the filepath, wildcards without
    values will be left as wildcards.  For example:
    
    ```
    assert expand("{a}_{b}.out", allow_missing=True) == ["{a}_{b}.out"]
    assert expand("{a}_{b}.out", a="test", allow_missing=True) == ["test_{b}.out"]
    assert expand("{a}.out", a="test", allow_missing=True) == ["test.out"]
    assert expand("{allow_missing}.out", allow_missing=True) == ["True.out"]
    assert expand("{a}.out", a="test", allow_missing="test2") == ["test.out"]
    with pytest.raises(WildcardError) as e:
        expand("{a}.out", allow_missing="test2")
    assert str(e.value) == "No values given for wildcard 'a'."
    ```
    all pass.
    
    Also:
    - Added unit tests for the expand function
    - Added to faq to describe overriding product in expand and the new
      allow_missing keyword.
    - Fixed some non-raw strings in re in io.py
    
    * Update FAQ
    
    Updated FAQ to be clearer (hopefully!)
    Changed rmarkdown yaml to pass test_issue635
    
    Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

--- snakemake/io.py
+++ snakemake/io.py
@@ -17,6 +17,7 @@ import functools
 import subprocess as sp
 from itertools import product, chain
 from contextlib import contextmanager
+import string
 import collections
 
 import yaml
@@ -892,7 +893,8 @@ def expand(*args, **wildcards):
         second arg (optional): a function to combine wildcard values
         (itertools.product per default)
     **wildcards -- the wildcards as keyword arguments
-        with their values as lists
+        with their values as lists. If allow_missing=True is included
+        wildcards in filepattern without values will stay unformatted.
     """
     filepatterns = args[0]
     if len(args) == 1:
@@ -916,12 +918,27 @@ def expand(*args, **wildcards):
             "of expand (e.g. 'temp(expand(\"plots/{sample}.pdf\", sample=SAMPLES))')."
         )
 
+    # check if remove missing is provided
+    format_dict = dict
+    if "allow_missing" in wildcards and wildcards["allow_missing"] is True:
+
+        class FormatDict(dict):
+            def __missing__(self, key):
+                return "{" + key + "}"
+
+        format_dict = FormatDict
+        # check that remove missing is not a wildcard in the filepatterns
+        for filepattern in filepatterns:
+            if "allow_missing" in re.findall(r"{([^}\.[!:]+)", filepattern):
+                format_dict = dict
+                break
+
     # remove unused wildcards to avoid duplicate filepatterns
     wildcards = {
         filepattern: {
             k: v
             for k, v in wildcards.items()
-            if k in re.findall("{([^}\.[!:]+)", filepattern)
+            if k in re.findall(r"{([^}\.[!:]+)", filepattern)
         }
         for filepattern in filepatterns
     }
@@ -934,11 +951,12 @@ def expand(*args, **wildcards):
                 values = [values]
             yield [(wildcard, value) for value in values]
 
+    formatter = string.Formatter()
     try:
         return [
-            filepattern.format(**comb)
+            formatter.vformat(filepattern, (), comb)
             for filepattern in filepatterns
-            for comb in map(dict, combinator(*flatten(wildcards[filepattern])))
+            for comb in map(format_dict, combinator(*flatten(wildcards[filepattern])))
         ]
     except KeyError as e:
         raise WildcardError("No values given for wildcard {}.".format(e))
@@ -1050,7 +1068,7 @@ def update_wildcard_constraints(
 
 
 def split_git_path(path):
-    file_sub = re.sub("^git\+file:/+", "/", path)
+    file_sub = re.sub(r"^git\+file:/+", "/", path)
     (file_path, version) = file_sub.split("@")
     file_path = os.path.realpath(file_path)
     root_path = get_git_root(file_path)
