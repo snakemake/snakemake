dc04db4a05fbdb7903aa8ba22043bc8d4ad2fb47
meesters <meesters@uni-mainz.de>
2020-01-23 12:27:20 +0100

    fix for #187: ensuring non conflicting loads

--- snakemake/deployment/env_modules.py
+++ snakemake/deployment/env_modules.py
@@ -9,12 +9,11 @@ class EnvModules:
         self.names = module_names
 
     def shellcmd(self, cmd):
-        """Return shell command with given modules loaded."""
-        loads = " ".join(self._load_module(name) for name in self.names)
-        return "{} {}".format(loads, cmd)
-
-    def _load_module(self, name):
-        return "module load {};".format(name)
+        """Return shell command with given modules to load, whilst
+           ensuring a clean environment.
+        """
+        to_load = " ".join(name for name in self.names)
+        return "module purge && module load {}".format(to_load)
 
     def __str__(self):
         return ", ".join(self.names)
