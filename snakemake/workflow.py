a1cc2eee9ede1a91e250515e4d20b850b9a28ba2
Johannes Köster <johannes.koester@uni-due.de>
2020-03-12 15:14:57 +0000

    Fix subworkflowconfig (#271)
    * This commit fixes the issue with running subworkflows without defined configfile (#42)
    
    * This commit fixes the issue with running subworkflows without defined
    configfile, the original fix was suggested by @wclose
    (see https://github.com/snakemake/snakemake/issues/24). A
    test for this case has been added to ensure future versions take this
    into account.
    
    * Update snakemake/workflow.py
    
    Co-Authored-By: Johannes Köster <johannes.koester@uni-due.de>
    
    Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>
    
    * fmt
    
    Co-authored-by: SultanOrazbayev <contact@econpoint.com>

--- snakemake/workflow.py
+++ snakemake/workflow.py
@@ -635,7 +635,9 @@ class Workflow:
                         subworkflow.snakefile,
                         workdir=subworkflow.workdir,
                         targets=subworkflow_targets,
-                        configfiles=[subworkflow.configfile],
+                        configfiles=[subworkflow.configfile]
+                        if subworkflow.configfile
+                        else None,
                         updated_files=updated,
                     ):
                         return False
