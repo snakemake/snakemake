dc59b7c896b4089296b62faa95f4f62f4a52c3b4
Vanessasaurus <vsochat@stanford.edu>
2020-03-16 15:02:37 -0600

    adding crc32c checksum for Google cloud storage (#273)
    The Google Storage api does not expose a function to stream to file,
    however the blob.write_to_file accepts a file object, so we can create
    a class to wrap this object, exposing the same functions, but also
    generating a hasher that we can update for each write action. This allows
    us to calculate the crc32c while the file is being downloaded and written
    to file as opposed to needing to download and then read again, which will
    make a difference for large files. See the docstrings in the GS.py remote
    file for links to further documentation
    
    Signed-off-by: vsoch <vsochat@stanford.edu>
    
    Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

--- setup.py
+++ setup.py
@@ -70,6 +70,12 @@ setup(
     extras_require={
         "reports": ["jinja2", "networkx", "pygments", "pygraphviz"],
         "messaging": ["slacker"],
+        "google-cloud": [
+            "crc32c",
+            "oauth2client",
+            "google-api-python-client",
+            "google-cloud-storage",
+        ],
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
