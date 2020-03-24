d4c36c215a0465f64327eab7c1a70fdae5da2fb9
Johannes Köster <johannes.koester@uni-due.de>
2020-03-24 10:41:50 +0100

    better error message

--- snakemake/linting/rules.py
+++ snakemake/linting/rules.py
@@ -56,7 +56,7 @@ class RuleLinter(Linter):
                 name = match.group("name")
                 if name not in valid_names:
                     yield Lint(
-                        title="Shell command directly uses variables from outside of the rule",
+                        title="Shell command directly uses variable {} from outside of the rule".format(name),
                         body="It is recommended to pass all files as input and output, and non-file parameters "
                         "via the params directive. Otherwise, provenance tracking is less accurate.",
                         links=[links.params],
