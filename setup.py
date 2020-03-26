290083a487e7cad6f8e8baa2cfbabb94d91e01c8
David Laehnemann <david.laehnemann@hhu.de>
2020-03-26 13:11:28 +0100

    ran black

--- setup.py
+++ setup.py
@@ -37,7 +37,13 @@ setup(
     zip_safe=False,
     license="MIT",
     url="https://snakemake.readthedocs.io",
-    packages=["snakemake", "snakemake.remote", "snakemake.report", "snakemake.caching", "snakemake.deployment"],
+    packages=[
+        "snakemake",
+        "snakemake.remote",
+        "snakemake.report",
+        "snakemake.caching",
+        "snakemake.deployment",
+    ],
     entry_points={
         "console_scripts": [
             "snakemake = snakemake:main",
@@ -60,7 +66,10 @@ setup(
         "nbformat",
         "toposort",
     ],
-    extras_require={"reports": ["jinja2", "networkx", "pygments", "pygraphviz"], "messaging": ["slacker"]},
+    extras_require={
+        "reports": ["jinja2", "networkx", "pygments", "pygraphviz"],
+        "messaging": ["slacker"],
+    },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
