9e2078515e762f1392494c1fd7f6eb078e1477fe
Michael Wilkinson <goi42@users.noreply.github.com>
2020-03-27 14:11:12 -0400

    apply black formatter

--- snakemake/remote/XRootD.py
+++ snakemake/remote/XRootD.py
@@ -154,26 +154,23 @@ class XRootDHelper(object):
 
     def _get_statinfo(self, url):
         domain, dirname, filename = self._parse_url(url)
-        matches = [
-            f for f in self.list_directory(domain, dirname) if f.name == filename
-        ]
+        matches = [f for f in self.list_directory(domain, dirname) if f.name == filename]
 
         assert len(matches) > 0
         if len(matches) > 1:
             # -- check matches for consistency
-            # There is a transient effect in XRootD 
+            # There is a transient effect in XRootD
             # where a file may match more than once.
             # This is okay as long as the statinfo
             # is the same for all of them.
             relevant_properties = [  # we only need to check front-facing attributes
-                x for x in dir(matches[0].statinfo) if not(x[:1] == '_' or x[-2:] == '__')
+                x for x in dir(matches[0].statinfo) if not (x[:1] == "_" or x[-2:] == "__")
             ]
             assert all(
-                    getattr(
-                        m.statinfo, p
-                    ) == getattr(
-                        matches[0].statinfo, p
-                    ) for m in matches[1:] for p in relevant_properties)
+                getattr(m.statinfo, p) == getattr(matches[0].statinfo, p)
+                for m in matches[1:]
+                for p in relevant_properties
+            )
 
         return matches[0].statinfo
 
