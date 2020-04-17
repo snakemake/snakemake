0d0cd2c9ec956ce461cb4e5ee2ea152fdd9c08f1
Marmaduke <jmeppley@gmail.com>
2020-04-17 17:58:20 -0400

    exapanded on error message to allow for checkpoint

--- snakemake/parser.py
+++ snakemake/parser.py
@@ -679,7 +679,8 @@ class Rule(GlobalKeywordState):
             for t in self.start():
                 yield t, token
         else:
-            self.error("Expected name or colon after rule keyword.", token)
+            self.error("Expected name or colon after "
+                       "rule or checkpoint  keyword.", token)
 
     def block_content(self, token):
         if is_name(token):
