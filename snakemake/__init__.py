7b343885d8bb430ebb4c794021fc7947b3dc65b8
Johannes Köster <johannes.koester@uni-due.de>
2020-04-21 09:50:05 +0100

    Optionally enable a more extensive conda cleanup, that also removes unused package caches. (#340)
    * allow to select caches as well for conda cleanupy
    
    * docs
    
    * docs
    
    * docs

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -6,7 +6,7 @@ __license__ = "MIT"
 import os
 import subprocess
 import glob
-from argparse import ArgumentError
+from argparse import ArgumentError, ArgumentDefaultsHelpFormatter
 import logging as _logging
 import re
 import sys
@@ -132,7 +132,7 @@ def snakemake(
     use_env_modules=False,
     singularity_args="",
     conda_prefix=None,
-    conda_cleanup_pkgs=False,
+    conda_cleanup_pkgs=None,
     list_conda_envs=False,
     singularity_prefix=None,
     shadow_prefix=None,
@@ -242,7 +242,8 @@ def snakemake(
         use_env_modules (bool):     load environment modules if defined in rules
         singularity_args (str):     additional arguments to pass to singularity
         conda_prefix (str):         the directory in which conda environments will be created (default None)
-        conda_cleanup_pkgs (bool):       whether to clean up conda tarballs after env creation (default False)
+        conda_cleanup_pkgs (snakemake.deployment.conda.CondaCleanupMode):
+                                    whether to clean up conda tarballs after env creation (default None), valid values: "tarballs", "cache"
         singularity_prefix (str):   the directory to which singularity images will be pulled (default None)
         shadow_prefix (str):        prefix for shadow directories. The job-specific shadow directories will be created in $SHADOW_PREFIX/shadow/ (default None)
         conda_create_envs_only (bool):    if specified, only builds the conda environments specified for each job, then exits.
@@ -813,6 +814,7 @@ def get_argument_parser(profile=None):
     parser = configargparse.ArgumentParser(
         description="Snakemake is a Python based language and execution "
         "environment for GNU Make-like workflows.",
+        formatter_class=ArgumentDefaultsHelpFormatter,
         default_config_files=config_files,
         config_file_parser_class=YAMLConfigFileParser,
     )
@@ -1767,10 +1769,18 @@ def get_argument_parser(profile=None):
         action="store_true",
         help="Cleanup unused conda environments.",
     )
+    from snakemake.deployment.conda import CondaCleanupMode
+
     group_conda.add_argument(
         "--conda-cleanup-pkgs",
-        action="store_true",
-        help="Cleanup conda packages after creating environments.",
+        type=CondaCleanupMode,
+        const=CondaCleanupMode.tarballs,
+        choices=list(CondaCleanupMode),
+        nargs="?",
+        help="Cleanup conda packages after creating environments. "
+        "In case of 'tarballs' mode, will clean up all downloaded package tarballs. "
+        "In case of 'cache' mode, will additionally clean up unused package caches. "
+        "If mode is omitted, will default to only cleaning up the tarballs.",
     )
     group_conda.add_argument(
         "--conda-create-envs-only",
