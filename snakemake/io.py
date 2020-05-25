cd0787e81dc21d17082c1f36d3a5fb1adb221692
jafors <forster_jan@web.de>
2020-05-25 09:44:39 +0200

    Reformatting

--- snakemake/io.py
+++ snakemake/io.py
@@ -971,12 +971,9 @@ def expand(*args, **wildcards):
 
 def multiext(prefix, *extensions):
     """Expand a given prefix with multiple extensions (e.g. .txt, .csv, _peaks.bed, ...)."""
-    if any(
-        (r"/" in ext or r"\\" in ext) for ext in extensions
-    ):
+    if any((r"/" in ext or r"\\" in ext) for ext in extensions):
         raise WorkflowError(
-            r"Extensions for multiext may not contain path delimiters "
-            r"(/,\)."
+            r"Extensions for multiext may not contain path delimiters " r"(/,\)."
         )
     return [flag(prefix + ext, "multiext", flag_value=prefix) for ext in extensions]
 
