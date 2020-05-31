ed601dbccd645da26e0ab86c57a0269f2277394e
Morten Enemark Lund <melund@gmail.com>
2020-05-31 09:29:43 +0200

    Pytest fixture to add a useable bash to path on windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import pytest
+from pathlib import Path
 
 from snakemake.common import ON_WINDOWS
+from snakemake.utils import find_bash_on_windows
 
 skip_on_windows = pytest.mark.skipif(ON_WINDOWS, reason="Unix stuff")
 
@@ -12,3 +15,14 @@ def reset_paths_between_tests():
     org_path = sys.path.copy()
     yield
     sys.path = org_path
+
+
+if ON_WINDOWS:
+
+    @pytest.fixture(autouse=True, scope="session")
+    def use_good_bash_on_windows(monkeypatch):
+        bash_cmd = find_bash_on_windows()
+        if bash_cmd:
+            bash_dir = Path(bash_cmd).parent
+            envpath = os.getenv("PATH")
+            monkeypatch.setenv("PATH", bash_dir + ";" + envpath)
