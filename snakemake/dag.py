cffabec75049b6757fbf04f5b9735e36048472bf
John Eppley <jmeppley@gmail.com>
2020-07-02 15:51:34 -0400

    reformatted with black

--- snakemake/dag.py
+++ snakemake/dag.py
@@ -227,10 +227,12 @@ class DAG:
                 self._jobid[job] = len(self._jobid)
 
     def cleanup_workdir(self):
-        for io_dir in set(os.path.dirname(io_file)
-                          for job in self.jobs
-                          for io_file in chain(job.output, job.input)
-                          if not os.path.exists(io_file)):
+        for io_dir in set(
+            os.path.dirname(io_file)
+            for job in self.jobs
+            for io_file in chain(job.output, job.input)
+            if not os.path.exists(io_file)
+        ):
             if os.path.exists(io_dir) and not len(os.listdir(io_dir)):
                 os.removedirs(io_dir)
 
