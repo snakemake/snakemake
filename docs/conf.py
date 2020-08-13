71571544ba7b5f7358dfcc0573f6a64eb680efdd
Johannes Köster <johannes.koester@uni-due.de>
2020-08-13 14:02:03 +0200

    explicitly add css path

--- docs/conf.py
+++ docs/conf.py
@@ -153,7 +153,7 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = ["_static/landingpage/index.html"]
+html_extra_path = ["_static/css"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
