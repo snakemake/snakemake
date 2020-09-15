fb504ac55f3c6c785881e4514ed1171ea8aab5d3
Johannes Köster <johannes.koester@uni-due.de>
2020-09-15 16:25:28 +0200

    pulp >=2.0 (#621)
    * pulp >=2.0
    
    * update changelog
    
    * change constraint name

--- snakemake/scheduler.py
+++ snakemake/scheduler.py
@@ -600,7 +600,7 @@ class JobScheduler:
                     [scheduled_jobs[job] * job.resources.get(name, 0) for job in jobs]
                 )
                 <= self.resources[name],
-                "Limitation of resource: {name}".format(name=name),
+                "limitation_of_resource_{name}".format(name=name),
             )
 
         # Choose jobs that lead to "fastest" (minimum steps) removal of existing temp file
