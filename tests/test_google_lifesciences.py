bf2a8993007e7f4300218ce16df541ec972e0bae
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2020-09-30 14:49:16 -0700

    create tests for issue #574 (checkpoints not working in cloud environments)

--- tests/test_google_lifesciences.py
+++ tests/test_google_lifesciences.py
@@ -90,3 +90,19 @@ def test_touch_remote_prefix():
         )
     finally:
         cleanup_google_storage(storage_prefix, bucket_name)
+
+@google_credentials
+def test_cloud_checkpoints_issue574():
+    storage_prefix = "snakemake-testing-%s" % next(tempfile._get_candidate_names())
+    workdir = dpath("test_cloud_checkpoints_issue574")
+    try:
+        run(
+            workdir,
+            use_conda=True,
+            default_remote_prefix="snakemake-testing/%s" % storage_prefix,
+            google_lifesciences=True,
+            google_lifesciences_cache=True,
+        )
+    finally:
+        cleanup_google_storage(storage_prefix)
+
