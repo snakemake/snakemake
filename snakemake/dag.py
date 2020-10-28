9e7b8fbf6ff7b91919dc9653389472cb11e15cd8
Johannes Köster <johannes.koester@tu-dortmund.de>
2020-10-28 12:00:15 +0100

    minor fix

--- snakemake/dag.py
+++ snakemake/dag.py
@@ -174,6 +174,8 @@ class DAG:
             self.targetjobs.add(job)
 
         for file in self.targetfiles:
+            # dbg
+            import sys
             print(file, file=sys.stderr)
             job = self.update(self.file2jobs(file), file=file, progress=progress)
             self.targetjobs.add(job)
