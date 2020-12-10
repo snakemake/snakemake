51405cc105e1d5d7e216833261e0c55011999ecb
Johannes Köster <johannes.koester@tu-dortmund.de>
2020-12-10 13:14:45 +0100

    set potential_new_ready_jobs in case of already processed job

--- snakemake/dag.py
+++ snakemake/dag.py
@@ -1138,6 +1138,7 @@ class DAG:
         candidate_groups = set()
         for job in jobs:
             if job in self._ready_jobs:
+                potential_new_ready_jobs = True
                 # job has been seen before, no need to process again
                 continue
             if not self.finished(job) and self._ready(job):
