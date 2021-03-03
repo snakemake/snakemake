21ac73c35441c069b20047c390f25cad8e414f68
Michael Hall <michael@mbh.sh>
2021-03-04 00:31:08 +1000

    Refactor f-strings (#898)
    * change f-strings to format strings
    
    * update changelog

--- snakemake/executors/__init__.py
+++ snakemake/executors/__init__.py
@@ -1559,7 +1559,7 @@ class KubernetesExecutor(ClusterExecutor):
                 if k in self.secret_files
             }
             for k, v in sorted(entry_sizes.items(), key=lambda item: item[1])[:-6:-1]:
-                logger.warning(f"  * File: {k}, original size: {v}")
+                logger.warning("  * File: {k}, original size: {v}".format(k=k, v=v))
 
             raise WorkflowError("ConfigMap too large")
 
