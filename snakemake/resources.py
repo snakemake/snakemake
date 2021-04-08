e44b4e210291281cb01a2e21cbb90deebb8bb96f
meesters <meesters@uni-mainz.de>
2021-04-08 16:40:01 +0200

    resources.py - now a flag like '--default-resources="account=m2_zdvhpc partition=smp"' can be parsed

--- snakemake/resources.py
+++ snakemake/resources.py
@@ -39,36 +39,37 @@ def parse_resources(resources_args, fallback=None):
     if resources_args is not None:
         valid = re.compile(r"[a-zA-Z_]\w*$")
         for res in resources_args:
-            try:
-                res, val = res.split("=")
-            except ValueError:
-                raise ValueError("Resources have to be defined as name=value pairs.")
-            if not valid.match(res):
-                raise ValueError(
-                    "Resource definition must start with a valid identifier."
+            for pair in re.split(r"[ \t,;]+", res):
+                try:
+                    key, val = pair.split("=")
+                except ValueError:
+                    raise ValueError("Resources have to be defined as name=value pairs.")
+                if not valid.match(key):
+                    raise ValueError(
+                        "Resource definition must start with a valid identifier."
                 )
 
-            # translate into supported type
-            if val in supported_keys:
+                # translate into supported type
+                if val in supported_keys:
+                    try:
+                        val = supported_keys[val]()
+                    except ValueError:
+                        raise ValueError(
+                              "Resource definition for '{}' requires an '{}'".format(val, supported_keys[val])
+                    )
+                # else fall back to ints
                 try:
-                    val = supported_keys[val]()
+                    val = int(val)
                 except ValueError:
+                    if fallback is not None:
+                        val = fallback(val)
+                    else:
+                        raise ValueError(
+                            "Resource definiton must contain an integer after the identifier."
+                        )
+                if key == "_cores":
                     raise ValueError(
-                       "Resource definition for '{}' requires an '{}'".format(val, supported_keys[val])
-                    )
-            # else fall back to ints
-            try:
-                val = int(val)
-            except ValueError:
-                if fallback is not None:
-                    val = fallback(val)
-                else:
-                    raise ValueError(
-                        "Resource definiton must contain an integer after the identifier."
-                    )
-            if res == "_cores":
-                raise ValueError(
-                    "Resource _cores is already defined internally. Use a different name."
+                         "Resource _cores is already defined internally. Use a different name."
                 )
-            resources[res] = val
+                resources[key] = val
     return resources
