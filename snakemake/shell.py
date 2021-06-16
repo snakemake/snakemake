69cc41141ca13bde93c0b5c279c0e89c7612aa03
Morten Enemark Lund <melund@gmail.com>
2021-06-16 13:35:08 +0200

    Win tests reset executable (#1048)
    * Reset shell.executable between widnows tests
    
    * tmp
    
    * tmp
    
    * Explicitly request bash when running tests on widnows
    
    * reset shell.executable between tests on windows

--- snakemake/shell.py
+++ snakemake/shell.py
@@ -13,7 +13,7 @@ import stat
 import tempfile
 import threading
 
-from snakemake.utils import format, argvquote, cmd_exe_quote, _find_bash_on_windows
+from snakemake.utils import format, argvquote, cmd_exe_quote, find_bash_on_windows
 from snakemake.common import ON_WINDOWS
 from snakemake.logging import logger
 from snakemake.deployment import singularity
@@ -64,7 +64,7 @@ class shell:
 
     @classmethod
     def executable(cls, cmd):
-        if os.name in ("posix", "nt") and not os.path.isabs(cmd):
+        if cmd and not os.path.isabs(cmd):
             # always enforce absolute path
             cmd = shutil.which(cmd)
             if not cmd:
@@ -73,9 +73,18 @@ class shell:
                     "is not available in your "
                     "PATH.".format(cmd)
                 )
-        if os.path.split(cmd)[-1].lower() in ("bash", "bash.exe"):
-            cls._process_prefix = "set -euo pipefail; "
-            cls._win_command_prefix = "-c"
+        if ON_WINDOWS:
+            if cmd is None:
+                cls._process_prefix = ""
+                cls._win_command_prefix = ""
+            elif os.path.split(cmd)[-1].lower() in ("bash", "bash.exe"):
+                if cmd == r"C:\Windows\System32\bash.exe":
+                    raise WorkflowError(
+                        "Cannot use WSL bash.exe on Windows. Ensure that you have "
+                        "a usable bash.exe availble on your path."
+                    )
+                cls._process_prefix = "set -euo pipefail; "
+                cls._win_command_prefix = "-c"
         cls._process_args["executable"] = cmd
 
     @classmethod
@@ -89,7 +98,7 @@ class shell:
     @classmethod
     def win_command_prefix(cls, cmd):
         """The command prefix used on windows when specifing a explicit
-        shell executable. This would be "-c" for bash and "/C" for cmd.exe
+        shell executable. This would be "-c" for bash.
         Note: that if no explicit executable is set commands are executed
         with Popen(..., shell=True) which uses COMSPEC on windows where this
         is not needed.
@@ -282,3 +291,5 @@ if os.name == "posix":
             shell.executable("sh")
     else:
         shell.executable("bash")
+elif ON_WINDOWS:
+    shell.executable(None)
