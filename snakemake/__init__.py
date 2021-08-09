f0c8e1d575e750fe333d53dceb269b5746f62a4c
kpj <kim.philipp.jablonski@gmail.com>
2021-08-09 12:28:41 +0200

    Use 'snakemake.utils.update_config' instead of 'dict.update' to update configs

--- snakemake/__init__.py
+++ snakemake/__init__.py
@@ -497,13 +497,13 @@ def snakemake(
         configfiles = []
     for f in configfiles:
         # get values to override. Later configfiles override earlier ones.
-        overwrite_config.update(load_configfile(f))
+        update_config(overwrite_config, load_configfile(f))
     # convert provided paths to absolute paths
     configfiles = list(map(os.path.abspath, configfiles))
 
     # directly specified elements override any configfiles
     if config:
-        overwrite_config.update(config)
+        update_config(overwrite_config, config)
         if config_args is None:
             config_args = unparse_config(config)
 
