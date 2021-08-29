452256ed466b9ae9544578c2a07e59ae10534bd5
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2021-08-29 08:07:24 -0400

    test_github_issue1158 mark xfail if no strace

--- tests/tests.py
+++ tests/tests.py
@@ -12,7 +12,7 @@ from pathlib import Path
 sys.path.insert(0, os.path.dirname(__file__))
 
 from .common import *
-from .conftest import skip_on_windows, only_on_windows, ON_WINDOWS
+from .conftest import skip_on_windows, only_on_windows, ON_WINDOWS, needs_strace
 
 
 def test_list_untracked():
@@ -1308,6 +1308,7 @@ def test_strict_mode():
     run(dpath("test_strict_mode"), shouldfail=True)
 
 
+@needs_strace
 def test_github_issue1158():
     run(
         dpath("test_github_issue1158"),
