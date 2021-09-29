3134ad3a5fd8a1627dc1899c06394fbdc6bec40c
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-09-29 15:52:18 -0700

    update test_cloud_checkpoints with new standards for gls

--- tests/test_google_lifesciences.py
+++ tests/test_google_lifesciences.py
@@ -91,18 +91,21 @@ def test_touch_remote_prefix():
     finally:
         cleanup_google_storage(storage_prefix, bucket_name)
 
+
 @google_credentials
 def test_cloud_checkpoints_issue574():
-    storage_prefix = "snakemake-testing-%s" % next(tempfile._get_candidate_names())
+    """see Github issue #574"""
+    bucket_name = "snakemake-testing-%s" % next(tempfile._get_candidate_names())
+    create_google_storage(bucket_name)
+    storage_prefix = "test_cloud_checkpoints_issue574"
     workdir = dpath("test_cloud_checkpoints_issue574")
     try:
         run(
             workdir,
             use_conda=True,
-            default_remote_prefix="snakemake-testing/%s" % storage_prefix,
+            default_remote_prefix="%s/%s" % (bucket_name, storage_prefix),
             google_lifesciences=True,
             google_lifesciences_cache=True,
         )
     finally:
-        cleanup_google_storage(storage_prefix)
-
+        cleanup_google_storage(storage_prefix, bucket_name)
