5cbefd22f6e4c19ea9c0bf4ce7f0c4fb49bdddd7
Don Freed <donfreed12@gmail.com>
2021-10-11 22:39:29 -0700

    Fix the first two MREs from #823.
    - Incorporates @epruesse's fix for MRE #1
    - Adds a fix for MRE #2 - properly marks group jobs as finished
    - Some minor updates to tests

--- snakemake/dag.py
+++ snakemake/dag.py
@@ -617,8 +617,13 @@ class DAG:
                 yield from filterfalse(partial(needed, job_), tempfiles & files)
 
             # temp output
-            if not job.dynamic_output and (
-                job not in self.targetjobs or job.rule.name == self.workflow.first_rule
+            if (
+                not job.dynamic_output
+                and not job.is_checkpoint
+                and (
+                    job not in self.targetjobs
+                    or job.rule.name == self.workflow.first_rule
+                )
             ):
                 tempfiles = (
                     f
@@ -1349,7 +1354,11 @@ class DAG:
         self._running.remove(job)
 
         # turn off this job's Reason
-        self.reason(job).mark_finished()
+        if job.is_group():
+            for j in job:
+                self.reason(j).mark_finished()
+        else:
+            self.reason(job).mark_finished()
 
         try:
             self._ready_jobs.remove(job)
