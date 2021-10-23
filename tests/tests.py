6b3daa04fb1ce2d6eae5a76c2217a09a7fdf55d0
Don Freed <donfreed12@gmail.com>
2021-10-22 22:09:01 -0700

    Skip MRE 2 from 823 on Windows due to `pipe()` output

--- tests/tests.py
+++ tests/tests.py
@@ -798,8 +798,12 @@ def test_issue805():
     run(dpath("test_issue805"), shouldfail=True)
 
 
-def test_issue823():
+def test_issue823_1():
     run(dpath("test_issue823_1"))
+
+
+@skip_on_windows
+def test_issue823_2():
     run(dpath("test_issue823_2"))
 
 
