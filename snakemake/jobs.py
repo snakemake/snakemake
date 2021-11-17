23346f2f554c88650057c56c57b84575cae84517
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-11-17 10:08:47 +0100

    fix code change checking when using modules

--- snakemake/jobs.py
+++ snakemake/jobs.py
@@ -244,13 +244,15 @@ class Job(AbstractJob):
             return
         if self.rule.basedir:
             # needed if rule is included from another subdirectory
-            path = os.path.relpath(os.path.join(self.rule.basedir, path))
-        assert os.path.exists(path), "cannot find {0}".format(path)
-        script_mtime = os.lstat(path).st_mtime
-        for f in self.expanded_output:
-            if f.exists:
-                if not f.is_newer(script_mtime):
-                    yield f
+            path = self.rule.basedir.join(path).get_path_or_uri()
+        if is_local_file(path):
+            assert os.path.exists(path), "cannot find {0}".format(path)
+            script_mtime = os.lstat(path).st_mtime
+            for f in self.expanded_output:
+                if f.exists:
+                    if not f.is_newer(script_mtime):
+                        yield f
+        # TODO also handle remote file case here.
 
     @property
     def threads(self):
