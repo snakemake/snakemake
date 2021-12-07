7b8e8fd2ad3a5b648c38e98f8c259ff0c524e4e0
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-12-07 17:08:25 +0100

    skip on windows

--- tests/tests.py
+++ tests/tests.py
@@ -1379,5 +1379,6 @@ def test_checkpoint_allowed_rules():
     run(dpath("test_checkpoint_allowed_rules"), targets=["c"], cluster="./qsub")
 
 
+@skip_on_windows
 def test_modules_ruledeps_inheritance():
     run(dpath("test_modules_ruledeps_inheritance"))
