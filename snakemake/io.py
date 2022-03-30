5b394c0319cfc5f8000b616d0f3c911a9091d05b
Johannes Köster <johannes.koester@tu-dortmund.de>
2022-03-30 14:23:37 +0200

    fix: better error messages in case of missing files after latency period (#1528)
    * fix: better error messages in case of missing files after latency period
    
    * fix syntax error

--- snakemake/io.py
+++ snakemake/io.py
@@ -824,10 +824,12 @@ def wait_for_files(
             if not get_missing():
                 return
             time.sleep(1)
+        missing = "\n".join(get_missing())
         raise IOError(
-            "Missing files after {} seconds:\n{}".format(
-                latency_wait, "\n".join(get_missing())
-            )
+            f"Missing files after {latency_wait} seconds. This might be due to "
+            "filesystem latency. If that is the case, consider to increase the "
+            "wait time with --latency-wait:\n"
+            f"{missing}"
         )
 
 
