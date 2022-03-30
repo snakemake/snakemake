d9a56aaf75c5f70ba0217d9d461d839fa3013f2e
Johannes Köster <johannes.koester@tu-dortmund.de>
2022-03-30 14:24:14 +0200

    fix: correct handling of exceptions in input functions that are generators (#1536)
    * fix: correct handling of exceptions in input functions that are generators
    
    * fmt

--- tests/tests.py
+++ tests/tests.py
@@ -1567,6 +1567,11 @@ def test_incomplete_params():
     run(dpath("test_incomplete_params"), dryrun=True, printshellcmds=True)
 
 
+@skip_on_windows
+def test_github_issue261():
+    run(dpath("test_github_issue261"), targets=["test1/target1/config1.done"])
+
+
 @skip_on_windows  # no pipe support on windows
 def test_pipe_depend():
     run(dpath("test_pipe_depend"), shouldfail=True)
