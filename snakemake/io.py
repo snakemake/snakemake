afbcb7ebb1e70e566582922973ed4be950f19ba2
Sichong Peng <scpeng@ucdavis.edu>
2022-04-19 20:16:22 -0700

    Remove unused import

--- snakemake/io.py
+++ snakemake/io.py
@@ -21,8 +21,6 @@ import string
 import collections
 import asyncio
 
-from numpy import character
-
 from snakemake.exceptions import (
     MissingOutputException,
     WorkflowError,
@@ -801,7 +799,6 @@ _illegal_wildcard_name_regex = re.compile(
     """,
     re.VERBOSE,
 )
-
 
 def wait_for_files(
     files, latency_wait=3, force_stay_on_remote=False, ignore_pipe_or_service=False
