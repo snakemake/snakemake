cff3121593db811687f82826da732e24f5e48e3f
Christian Meesters <meesters@uni-mainz.de>
2022-05-02 20:24:22 +0200

    trying different test approach

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shlex
 
 sys.path.insert(0, os.path.dirname(__file__))
 
@@ -7,12 +8,13 @@ from common import *
 
 def test_slurm():
     workdir = dpath("test_slurm")
+    # first, we need to copy the Snakefile to the "cluster"
+    cp_cmd = "vagrant scp Snakefile controller:."
+    # this is the command to be tested:
+    cmd = "vagrant ssh controller -- -t 'snakemake --use-envmodules --slurm -j 1 -F'"
+    # if our workflow was sucessful the 'pi.calc'-file should be present.
+    check_cmd = "vagrant scp controller:pi.calc ."
 
-    run(
-        workdir,
-        use_conda=False,
-        # not using env_modules in this context, either
-        # MPI is supposted to be installed, nothing
-        # else is needed in the vagrant setup.
-        slurm=True,
-    )
+    subprocess.check_call(shlex.split(cp_cmd))
+    subprocess.check_call(shlex.split(cmd))
+    subprocess.check_call(shlex.split(check_cmd))
