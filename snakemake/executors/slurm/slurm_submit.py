6c53a01a27ab9d7598e2823549118492ff5345ed
meesters <meesters@uni-mainz.de>
2022-06-15 12:27:15 +0200

    formatted correctly

--- snakemake/executors/slurm/slurm_submit.py
+++ snakemake/executors/slurm/slurm_submit.py
@@ -140,9 +140,9 @@ class SlurmExecutor(ClusterExecutor):
             )
             sys.exit(1)
 
-        if job.resources.get('account'):
+        if job.resources.get("account"):
             call += " -A {account}".format(**job.resources)
-        if job.resources.get('partition')
+        if job.resources.get("partition"):
             call += " -p {partition}".format(**job.resources)
 
         if not job.resources.get("walltime_minutes"):
