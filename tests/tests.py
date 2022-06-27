05f1f845a1e5f6c57ff4a78286d55f05c0bca98f
kpj <kim.philipp.jablonski@gmail.com>
2022-06-27 15:12:37 +0200

    Add test

--- tests/tests.py
+++ tests/tests.py
@@ -1517,6 +1517,11 @@ def test_conda_named():
 
 
 @skip_on_windows
+def test_conda_function():
+    run(dpath("test_conda_function"), use_conda=True)
+
+
+@skip_on_windows
 def test_default_target():
     run(dpath("test_default_target"))
 
