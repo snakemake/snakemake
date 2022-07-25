422955a7acf1f022c064224ffc9baa2fc1073b9b
meesters <meesters@uni-mainz.de>
2022-07-25 10:11:24 +0200

    removed FIXME statement and code, due to the fix in 2eb66fbc53e9e9bad2c5de43566ead81767d1922

--- snakemake/executors/slurm/slurm_submit.py
+++ snakemake/executors/slurm/slurm_submit.py
@@ -292,8 +292,4 @@ class SlurmExecutor(ClusterExecutor):
 
             with self.lock:
                 self.active_jobs.extend(still_running)
-            # FIXME: check why self.max_status_checks_per_second is not an attribute of this class!
-            # This code is not working for reasons unkown!
-            # Hence, the hardcoded drop-in.
-            # time.sleep(1 / self.max_status_checks_per_second)
-            time.sleep(30)
+            time.sleep(1 / self.max_status_checks_per_second)
