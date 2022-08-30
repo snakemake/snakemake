7d2c0c3dd663687a228a80277c5c7c525ce71fdc
Egor Kosaretskiy <megametagross@outlook.de>
2022-08-30 14:14:17 +0300

    added test for conda env inside script

---
+++ tests/test_prebuilt_conda_script/test.py
@@ -0,0 +1,4 @@
+import luigi
+
+with open('output.txt', 'w') as file:
+	file.write("something\n")
--- tests/tests.py
+++ tests/tests.py
@@ -1926,3 +1926,8 @@ def test_github_issue1618():
 
 def test_conda_python_script():
     run(dpath("test_conda_python_script"), use_conda=True)
+
+def test_prebuilt_conda_script():
+    sp.run('conda create -n test_prebuilt_conda_script python=3.9 -y', shell=True)
+    sp.run('conda install -n test_prebuilt_conda_script luigi=3.0.3 -y', shell=True)
+    run(dpath("test_prebuilt_conda_script"), use_conda=True)
