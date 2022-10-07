326ba48566c798731bfbf75e46faac2e4edee972
Rodrigo Luger <rodluger@gmail.com>
2022-10-07 11:22:38 -0400

    Comparison of rules and non-rule instances

--- snakemake/rules.py
+++ snakemake/rules.py
@@ -1219,7 +1219,10 @@ class Rule:
         return self.name.__hash__()
 
     def __eq__(self, other):
-        return self.name == other.name and self.output == other.output
+        if type(other) is type(self):
+            return self.name == other.name and self.output == other.output
+        else:
+            return False
 
 
 class Ruleorder:
