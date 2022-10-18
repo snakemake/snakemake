snakemake/snakemake

commit 552fadf7d0ea0f0ff3990295db2618a31c692f61

fix: avoid "Admin" prompt when using conda on windows (#1046)
* Skip menu shortcut installation for conda environments

At least on Windows, some packages will try to install start-menu
shortcuts which require admin rights. This prevent an unattended
installation of the conda environment.

There is a "--no-shortcuts"
 for "conda create", but that doesn't work for "conda env create"

 See: https://github.com/conda/conda/issues/9474

 Conda build handles the problem by creating environments prefixed with
 an underscore "_<env-name>", which also cause conda to skip environment
 creation.

* Update environment names with underscore prefix

* fmt

* fix variable name

Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>
Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>

snakemake/deployment/conda.py
@@ -239,13 +239,16 @@ class Env:
             hash_candidates = [
                 hash[:8],
                 hash,
+                hash
+                + "_",  # activate no-shortcuts behavior (so that no admin rights are needed on win)
             ]  # [0] is the old fallback hash (shortened)
             exists = [os.path.exists(get_path(h)) for h in hash_candidates]
-            if self.is_containerized or exists[1] or (not exists[0]):
-                # containerizes, full hash exists or fallback hash does not exist: use full hash
+            if self.is_containerized:
                 return get_path(hash_candidates[1])
-            # use fallback hash
-            return get_path(hash_candidates[0])
+            for candidate, candidate_exists in zip(hash_candidates, exists):
+                if candidate_exists or candidate == hash_candidates[-1]:
+                    # exists or it is the last (i.e. the desired one)
+                    return get_path(candidate)
 
     @property
     def address_argument(self):
@@ -479,6 +482,7 @@ class Env:
                             "conda",
                             "create",
                             "--quiet",
+                            "--no-shortcuts",
                             "--yes",
                             "--prefix '{}'".format(env_path),
                         ]

tests/test_containerized/Dockerfile
@@ -6,16 +6,16 @@ LABEL io.github.snakemake.conda_env_hash="d630028d41ac6564d03ea65c4edb2d271d33f3
 
 # Conda environment:
 #   source: env.yaml
-#   prefix: /conda-envs/1219696ceec39be99ff366456467efb2
+#   prefix: /conda-envs/_1219696ceec39be99ff366456467efb2
 #   channels:
 #     - bioconda
 #     - conda-forge
 #   dependencies:
 #     - bcftools =1.10
-RUN mkdir -p /conda-envs/1219696ceec39be99ff366456467efb2
-COPY env.yaml /conda-envs/1219696ceec39be99ff366456467efb2/environment.yaml
+RUN mkdir -p /conda-envs/_1219696ceec39be99ff366456467efb2
+COPY env.yaml /conda-envs/_1219696ceec39be99ff366456467efb2/environment.yaml
 
 # Step 2: Generate conda environments
 
-RUN mamba env create --prefix /conda-envs/1219696ceec39be99ff366456467efb2 --file /conda-envs/1219696ceec39be99ff366456467efb2/environment.yaml && \
+RUN mamba env create --prefix /conda-envs/_1219696ceec39be99ff366456467efb2 --file /conda-envs/_1219696ceec39be99ff366456467efb2/environment.yaml && \
     mamba clean --all -y

commit 8a86a1e5ec7438492e2f1403b1b0fd81030255ad

fix: conda create --no-shortcuts absent on Linux/MacOS (regression from #1046) (#1916)
`conda create --no-shortcuts` is only valid on windows. The option does
not exist on other OSes. This fixes the regression introduced in #1046

See
https://github.com/conda/conda/blob/32b958df05e5fab04254ed2af9044ea99f5f13f1/conda/cli/conda_argparse.py#L1827-L1837

snakemake/deployment/conda.py
@@ -492,7 +492,7 @@ class Env:
                             "conda",
                             "create",
                             "--quiet",
-                            "--no-shortcuts",
+                            "--no-shortcuts" if ON_WINDOWS else "",
                             "--yes",
                             "--prefix '{}'".format(env_path),
                         ]