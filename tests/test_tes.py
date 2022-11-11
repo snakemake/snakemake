504d26cd87f508ef85a26729b66e1126059279dc
Sven Twardziok <sven.twardziok@charite.de>
2022-11-11 09:42:50 +0100

    again formatting

--- tests/test_tes.py
+++ tests/test_tes.py
@@ -16,9 +16,9 @@ TEST_POST_RESPONSE = {"id": "id_1"}
 TEST_TASK = {"id": "id_1", "state": "COMPLETE"}
 
 TES_TOKEN = (
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
-    "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ." +
-    "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+    + "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ."
+    + "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 )
 
 def _validate_task(task):
