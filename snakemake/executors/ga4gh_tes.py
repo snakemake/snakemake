a30b07569f0a9949a058a6ddd303e265a82eb568
Sven Twardziok <sven.twardziok@bih-charite.de>
2022-11-23 16:46:49 +0100

    simplify

--- snakemake/executors/ga4gh_tes.py
+++ snakemake/executors/ga4gh_tes.py
@@ -49,8 +49,7 @@ class TaskExecutionServiceExecutor(ClusterExecutor):
         self.container_workdir = "/tmp"
         self.max_status_checks_per_second = max_status_checks_per_second
         self.tes_url = tes_url
-        self.tes_token = os.environ.get("TES_TOKEN")
-        self.tes_client = tes.HTTPClient(url=self.tes_url, token=self.tes_token)
+        self.tes_client = tes.HTTPClient(url=self.tes_url, token=os.environ.get("TES_TOKEN"))
 
         logger.info("[TES] Job execution on TES: {url}".format(url=self.tes_url))
 
