1be511e9523813e488c396c9b8f1b668a7c61bba
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2023-02-10 21:20:56 -0800

    reproduce issue 2021 with MRE

--- tests/test_google_lifesciences.py
+++ tests/test_google_lifesciences.py
@@ -168,3 +168,22 @@ def test_github_issue1460():
         )
     finally:
         cleanup_google_storage(storage_prefix, bucket_name)
+
+@google_credentials
+def test_cloud_checkpoints_issue2021():
+    """see Github issue #2021"""
+    bucket_name = get_temp_bucket_name()
+    create_google_storage(bucket_name)
+    storage_prefix = "test_cloud_checkpoints_issue2021"
+    workdir = dpath("test_cloud_checkpoints_issue2021")
+    try:
+        run(
+            workdir,
+            use_conda=True,
+            default_remote_prefix="%s/%s" % (bucket_name, storage_prefix),
+            google_lifesciences=True,
+            google_lifesciences_cache=False,
+        )
+    finally:
+        cleanup_google_storage(storage_prefix, bucket_name)
+
