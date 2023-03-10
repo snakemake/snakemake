24ca47e750a5e68da21e62001993269861fa5e4b
Johannes Köster <johannes.koester@uni-due.de>
2023-03-10 17:33:08 +0100

    also mimic account query

--- .github/workflows/scripts/sacct-proxy.py
+++ .github/workflows/scripts/sacct-proxy.py
@@ -9,7 +9,17 @@ parser.add_argument("--format")
 parser.add_argument("-X", action="store_true")
 parser.add_argument("--parsable2", action="store_true")
 parser.add_argument("--noheader", action="store_true")
+parser.add_argument("-n", action="store_true")
+parser.add_argument("-u")
+parser.add_argument("-o")
+
 
 args = parser.parse_args()
 
-sp.call(["squeue", "--noheader", "--format", "%F|%T", "--name", args.name])
+if args.n and args.u and args.o:
+    # mimic account query from the executor
+    print("runner")
+elif args.name:
+    sp.call(["squeue", "--noheader", "--format", "%F|%T", "--name", args.name])
+else:
+    raise ValueError("Unsupported arguments")
