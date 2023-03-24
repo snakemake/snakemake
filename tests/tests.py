215ec20b35e81104b8baf2c7a4f419606a6b063e
Sichong Peng <scpeng@ucdavis.edu>
2023-03-23 22:02:26 -0700

    Add a test for issue 2142

--- tests/tests.py
+++ tests/tests.py
@@ -1961,6 +1961,8 @@ def test_cleanup_metadata_fail():
 def test_github_issue1389():
     run(dpath("test_github_issue1389"), resources={"foo": 4}, shouldfail=True)
 
+def test_github_issue2142():
+    run(dpath("test_github_issue2142"))
 
 def test_ensure_nonempty_fail():
     run(dpath("test_ensure"), targets=["a"], shouldfail=True)
