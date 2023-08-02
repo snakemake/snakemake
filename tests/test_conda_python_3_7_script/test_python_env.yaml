snakemake/snakemake

commit e9f6731858ba851b689236d89c26e739852fa3d6

refactor: define consistent abstract base classes for jobs (#2341)
### Description

<!--Add a description of your PR here-->

### QC
<!-- Make sure that you can tick the boxes below. -->

* [x] The PR contains a test case for the changes or the changes are
already covered by an existing test case.
* [x] The documentation (`docs/`) is updated to reflect the changes or
this is not necessary (e.g. if the change does neither modify the
language nor the behavior or functionalities of Snakemake).

setup.cfg
@@ -22,8 +22,6 @@ classifiers =
     License :: OSI Approved :: MIT License
     Natural Language :: English
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.7
-    Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
     Programming Language :: Python :: 3.11
@@ -33,7 +31,7 @@ classifiers =
 zip_safe = False
 include_package_data = False
 packages = find:
-python_requires = >=3.7
+python_requires = >=3.9
 install_requires =
     appdirs
     configargparse

snakemake/__init__.py
@@ -3,13 +3,17 @@ __copyright__ = "Copyright 2022, Johannes Köster"
 __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
 
+import sys
+
+if sys.version_info < (3, 9):
+    raise ValueError("Snakemake requires at least Python 3.9.")
+
 import os
 import glob
 from argparse import ArgumentDefaultsHelpFormatter
 import logging as _logging
 from pathlib import Path
 import re
-import sys
 import threading
 import webbrowser
 from functools import partial
@@ -648,6 +652,8 @@ def snakemake(
             local_groupid=local_groupid,
             keep_metadata=keep_metadata,
             latency_wait=latency_wait,
+            cleanup_scripts=cleanup_scripts,
+            immediate_submit=immediate_submit,
         )
         success = True
 
@@ -818,7 +824,6 @@ def snakemake(
                     max_jobs_per_second=max_jobs_per_second,
                     max_status_checks_per_second=max_status_checks_per_second,
                     printd3dag=printd3dag,
-                    immediate_submit=immediate_submit,
                     ignore_ambiguity=ignore_ambiguity,
                     stats=stats,
                     force_incomplete=force_incomplete,
@@ -845,7 +850,6 @@ def snakemake(
                     conda_cleanup_envs=conda_cleanup_envs,
                     cleanup_containers=cleanup_containers,
                     cleanup_shadow=cleanup_shadow,
-                    cleanup_scripts=cleanup_scripts,
                     subsnakemake=subsnakemake,
                     updated_files=updated_files,
                     allowed_rules=allowed_rules,
@@ -3168,7 +3172,6 @@ def main(argv=None):
             conda_cleanup_envs=args.conda_cleanup_envs,
             cleanup_containers=args.cleanup_containers,
             cleanup_shadow=args.cleanup_shadow,
-            cleanup_scripts=not args.skip_script_cleanup,
             force_incomplete=args.rerun_incomplete,
             ignore_incomplete=args.ignore_incomplete,
             list_version_changes=args.list_version_changes,
@@ -3237,6 +3240,7 @@ def main(argv=None):
             scheduler_solver_path=args.scheduler_solver_path,
             conda_base_path=args.conda_base_path,
             local_groupid=args.local_groupid,
+            cleanup_scripts=not args.skip_script_cleanup,
         )
 
     if args.runtime_profile:

snakemake/interfaces.py
@@ -1,0 +1,495 @@
+__author__ = "Johannes Köster"
+__copyright__ = "Copyright 2023, Johannes Köster"
+__email__ = "johannes.koester@uni-due.de"
+__license__ = "MIT"
+
+import sys
+
+# The typing API used here requires Python 3.9 or later and fails with unintuitive errors otherwise.
+if sys.version_info < (3, 9):
+    raise ValueError(
+        "Snakemake executor interface definition requires at least Python 3.9."
+    )
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class ExecutorJobInterface(ABC):
+    @property
+    @abstractmethod
+    def name(self):
+        ...
+
+    @property
+    @abstractmethod
+    def jobid(self):
+        ...
+
+    @abstractmethod
+    def logfile_suggestion(self, prefix: str) -> str:
+        ...
+
+    @abstractmethod
+    def is_group(self):
+        ...
+
+    @abstractmethod
+    def log_info(self, skip_dynamic=False):
+        ...
+
+    @abstractmethod
+    def log_error(self, msg=None, **kwargs):
+        ...
+
+    @abstractmethod
+    def remove_existing_output(self):
+        ...
+
+    @abstractmethod
+    def download_remote_input(self):
+        ...
+
+    @abstractmethod
+    def properties(self, omit_resources=["_cores", "_nodes"], **aux_properties):
+        ...
+
+    @property
+    @abstractmethod
+    def resources(self):
+        ...
+
+    @abstractmethod
+    def check_protected_output(self):
+        ...
+
+    @property
+    @abstractmethod
+    def is_local(self):
+        ...
+
+    @property
+    @abstractmethod
+    def is_branched(self):
+        ...
+
+    @property
+    @abstractmethod
+    def is_updated(self):
+        ...
+
+    @property
+    @abstractmethod
+    def output(self):
+        ...
+
+    @abstractmethod
+    def register(self):
+        ...
+
+    @abstractmethod
+    def postprocess(self):
+        ...
+
+    @abstractmethod
+    def get_target_spec(self):
+        ...
+
+    @abstractmethod
+    def rules(self):
+        ...
+
+    @property
+    @abstractmethod
+    def attempt(self):
+        ...
+
+    @property
+    @abstractmethod
+    def input(self):
+        ...
+
+    @property
+    @abstractmethod
+    def threads(self) -> int:
+        ...
+
+    @property
+    @abstractmethod
+    def resources(self):
+        ...
+
+    @property
+    @abstractmethod
+    def log(self):
+        ...
+
+    @abstractmethod
+    def cleanup(self):
+        ...
+
+    @abstractmethod
+    def get_wait_for_files(self):
+        ...
+
+    @abstractmethod
+    def format_wildcards(self, string, **variables):
+        ...
+
+    @property
+    @abstractmethod
+    def needs_singularity(self):
+        ...
+
+
+class SingleJobExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def rule(self):
+        ...
+
+    @abstractmethod
+    def prepare(self):
+        ...
+
+    @property
+    @abstractmethod
+    def conda_env(self):
+        ...
+
+    @property
+    @abstractmethod
+    def container_img_path(self):
+        ...
+
+    @property
+    @abstractmethod
+    def env_modules(self):
+        ...
+
+    @property
+    @abstractmethod
+    def benchmark_repeats(self):
+        ...
+
+    @property
+    @abstractmethod
+    def benchmark(self):
+        ...
+
+    @property
+    @abstractmethod
+    def params(self):
+        ...
+
+    @property
+    @abstractmethod
+    def wildcards(self):
+        ...
+
+    @property
+    @abstractmethod
+    def shadow_dir(self):
+        ...
+
+    @property
+    @abstractmethod
+    def is_shadow(self):
+        ...
+
+    @property
+    @abstractmethod
+    def is_run(self):
+        ...
+
+    @property
+    @abstractmethod
+    def is_template_engine(self):
+        ...
+
+    @property
+    @abstractmethod
+    def message(self):
+        ...
+
+
+class GroupJobExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def jobs(self):
+        ...
+
+    @property
+    @abstractmethod
+    def groupid(self):
+        ...
+
+    @property
+    @abstractmethod
+    def toposorted(self):
+        ...
+
+
+class DAGExecutorInterface(ABC):
+    @abstractmethod
+    def is_edit_notebook_job(self, job: ExecutorJobInterface):
+        ...
+
+    @abstractmethod
+    def incomplete_external_jobid(self, job: ExecutorJobInterface):
+        ...
+
+    @abstractmethod
+    def jobid(self, job: ExecutorJobInterface):
+        ...
+
+    @abstractmethod
+    def get_sources(self):
+        ...
+
+
+class JobSchedulerExecutorInterface(ABC):
+    @abstractmethod
+    def executor_error_callback(self, exception):
+        ...
+
+
+class PersistenceExecutorInterface(ABC):
+    @abstractmethod
+    def cleanup(self):
+        ...
+
+    @property
+    @abstractmethod
+    def path(self):
+        ...
+
+    @property
+    @abstractmethod
+    def aux_path(self):
+        ...
+
+
+class WorkflowExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def latency_wait(self) -> int:
+        ...
+
+    @property
+    @abstractmethod
+    def rerun_triggers(self) -> Optional[list[str]]:
+        ...
+
+    @property
+    @abstractmethod
+    def shadow_prefix(self) -> Optional[str]:
+        ...
+
+    @property
+    @abstractmethod
+    def conda_frontend(self) -> Optional[str]:
+        ...
+
+    @property
+    @abstractmethod
+    def conda_prefix(self) -> Optional[str]:
+        ...
+
+    @property
+    @abstractmethod
+    def conda_base_path(self) -> Optional[str]:
+        ...
+
+    @property
+    @abstractmethod
+    def singularity_args(self) -> Optional[str]:
+        ...
+
+    @property
+    @abstractmethod
+    def execute_subworkflows(self) -> bool:
+        ...
+
+    @property
+    @abstractmethod
+    def max_threads(self) -> Optional[int]:
+        ...
+
+    @property
+    @abstractmethod
+    def keep_metadata(self) -> bool:
+        ...
+
+    @property
+    @abstractmethod
+    def wrapper_prefix(self) -> Optional[str]:
+        ...
+
+    @property
+    @abstractmethod
+    def overwrite_threads(self) -> dict[str, int]:
+        ...
+
+    @property
+    @abstractmethod
+    def overwrite_scatter(self) -> dict[str, int]:
+        ...
+
+    @property
+    @abstractmethod
+    def local_groupid(self):
+        ...
+
+    @property
+    @abstractmethod
+    def conda_not_block_search_path_envvars(self):
+        ...
+
+    @property
+    @abstractmethod
+    def overwrite_configfiles(self):
+        ...
+
+    @property
+    @abstractmethod
+    def config_args(self):
+        ...
+
+    @property
+    @abstractmethod
+    def printshellcmds(self):
+        ...
+
+    @property
+    @abstractmethod
+    def scheduler_type(self):
+        ...
+
+    @property
+    @abstractmethod
+    def overwrite_resources(self):
+        ...
+
+    @property
+    @abstractmethod
+    def default_resources(self):
+        ...
+
+    @property
+    @abstractmethod
+    def overwrite_resource_scopes(self):
+        ...
+
+    @property
+    @abstractmethod
+    def resource_scopes(self):
+        ...
+
+    @abstractmethod
+    def get_cache_mode(self, rule):
+        ...
+
+    @property
+    @abstractmethod
+    def output_file_cache(self):
+        ...
+
+    @property
+    @abstractmethod
+    def main_snakefile(self):
+        ...
+
+    @property
+    @abstractmethod
+    def persistence(self) -> PersistenceExecutorInterface:
+        ...
+
+    @property
+    @abstractmethod
+    def keep_metadata(self):
+        ...
+
+    @property
+    @abstractmethod
+    def linemaps(self):
+        ...
+
+    @property
+    @abstractmethod
+    def workdir_init(self):
+        ...
+
+    @property
+    @abstractmethod
+    def use_conda(self):
+        ...
+
+    @property
+    @abstractmethod
+    def use_singularity(self):
+        ...
+
+    @property
+    @abstractmethod
+    def use_env_modules(self):
+        ...
+
+    @property
+    @abstractmethod
+    def debug(self):
+        ...
+
+    @property
+    @abstractmethod
+    def cleanup_scripts(self):
+        ...
+
+    @property
+    @abstractmethod
+    def edit_notebook(self):
+        ...
+
+    @property
+    @abstractmethod
+    def sourcecache(self):
+        ...
+
+    @property
+    @abstractmethod
+    def verbose(self):
+        ...
+
+    @property
+    @abstractmethod
+    def jobscript(self):
+        ...
+
+    @property
+    @abstractmethod
+    def envvars(self):
+        ...
+
+    @property
+    @abstractmethod
+    def scheduler(self) -> JobSchedulerExecutorInterface:
+        ...
+
+    @property
+    @abstractmethod
+    def immediate_submit(self):
+        ...
+
+    @property
+    @abstractmethod
+    def default_remote_prefix(self):
+        ...
+
+    @property
+    @abstractmethod
+    def rules(self):
+        ...
+
+    @abstractmethod
+    def get_rule(self, name):
+        ...

test-environment.yml
@@ -2,7 +2,7 @@ channels:
   - conda-forge
   - bioconda
 dependencies:
-  - python
+  - python >=3.9
   - yte
   - packaging
   - stopit

tests/test_singularity/Snakefile
@@ -2,9 +2,9 @@
 
 rule a:
     input:
-        "test.txt"
+        "test.txt",
     output:
-        "test.out"
+        "test.out",
     singularity:
         "docker://bash"
     shell:
@@ -13,16 +13,16 @@ rule a:
 
 rule b:
     output:
-        "test.txt"
+        "test.txt",
     singularity:
-        "docker://python:3.8.3"
+        "docker://python:3.9"
     script:
         "script.py"
 
 
 rule c:
     output:
-        "invalid.txt"
+        "invalid.txt",
     singularity:
         "docker://invalid/image url even with whitespace"
     shell:

.github/workflows/main.yml
@@ -245,16 +245,19 @@ jobs:
       #  run: |
       #    pytest -s -v -x tests/test_kubernetes.py
 
-      - name: Test Azure Batch Executor
-        shell: bash -el {0}
-        env:
-          AZ_BLOB_PREFIX: "${{ secrets.AZ_BLOB_PREFIX }}"
-          AZ_BLOB_ACCOUNT_URL: "${{ secrets.AZ_STORAGE_ACCOUNT_URL }}"
-          AZ_BLOB_CREDENTIAL: "${{ secrets.AZ_STORAGE_KEY }}"
-          AZ_BATCH_ACCOUNT_URL: "${{ secrets.AZ_BATCH_ACCOUNT_URL }}"
-          AZ_BATCH_ACCOUNT_KEY: "${{ secrets.AZ_BATCH_KEY }}"
-        run: |
-          pytest -s -v -x tests/test_azure_batch_executor.py
+      # TODO temporarily disable testing of the azure batch executor as our azure
+      # account is currently disabled for unknown reason.
+      # Reactivate once that is fixed.
+      # - name: Test Azure Batch Executor
+      #   shell: bash -el {0}
+      #   env:
+      #     AZ_BLOB_PREFIX: "${{ secrets.AZ_BLOB_PREFIX }}"
+      #     AZ_BLOB_ACCOUNT_URL: "${{ secrets.AZ_STORAGE_ACCOUNT_URL }}"
+      #     AZ_BLOB_CREDENTIAL: "${{ secrets.AZ_STORAGE_KEY }}"
+      #     AZ_BATCH_ACCOUNT_URL: "${{ secrets.AZ_BATCH_ACCOUNT_URL }}"
+      #     AZ_BATCH_ACCOUNT_KEY: "${{ secrets.AZ_BATCH_KEY }}"
+      #   run: |
+      #     pytest -s -v -x tests/test_azure_batch_executor.py
 
       - name: Test GA4GH TES executor
         shell: bash -el {0}

.github/workflows/test-flux.yaml
@@ -1,6 +1,7 @@
 name: Test Flux Executor
-on:
-  pull_request: []
+# TODO temporarily disable flux executor tests as the test container is based on Python 3.8 but Snakemake now requires Python 3.9
+# on:
+#   pull_request: []
 
 jobs:
   build:
@@ -26,14 +27,23 @@ jobs:
       - name: Checkout
         uses: actions/checkout@v3
 
+      - name: Setup miniconda
+        uses: conda-incubator/setup-miniconda@v2
+        with:
+          activate-environment: snakemake
+          channels: "conda-forge, bioconda"
+          miniforge-variant: Mambaforge
+          miniforge-version: latest
+
       - name: Install Snakemake
+        shell: bash -el {0}
         run: |
-          wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh           
-          /bin/bash ~/miniconda.sh -b -p /opt/conda
-          # Install snakemake
+          conda config --set channel_priority strict
+          mamba install python>=3.9 pip
           pip install .
 
       - name: Start Flux and Test Workflow
+        shell: bash -el {0}
         run: |
           # We must have python3->python accessible for this to work
           ln -s /bin/python3 /usr/local/bin/python
@@ -44,9 +54,8 @@ jobs:
           flux start snakemake --show-failed-logs --verbose --flux --jobs=1 --no-shared-fs
 
       - name: Test Flux with Conda
+        shell: bash -el {0}
         run: |
-          # conda python needs to be after system python
-          export PATH=$PATH:/opt/conda/bin
           which python
           which conda
           cp -R ./tests/test_conda /tmp/test_conda

commit 0d0e9c4cf48a97952464e6da476ed7661d629ce3

fix: require python >=3.7 again (the python 3.9 dependency was unnecessary) (#2372)
### Description

The respective bug was introduced with PR #2341, so it exists starting
with version `7.30.2`. Also see issue #2369 and PR #2370 for further
context.

This code from PR #2341 seems to get run every time a conda environment
is used:


https://github.com/snakemake/snakemake/blob/13b5d4d11d7475f942531c51b6013962a88a72b7/snakemake/__init__.py#L8-L9

Thus, any conda environment in a rule with an older python version
included, will trigger a:
```
  File "/opt/conda/envs/snakemake/lib/python3.11/site-packages/snakemake/__init__.py", line 9, in <module>
    raise ValueError("Snakemake requires at least Python 3.9.")
ValueError: Snakemake requires at least Python 3.9.
```

This would preclude snakemake from running any software that needs an
older python version, so this is clearly an unwanted side-effect. But I
am not sure how to fix this.

Currently, this PR is only a minimal example test to trigger this
behaviour in the CI tests. Hopefully, this will make debugging it
quicker for @johanneskoester (or someone else?).

### QC
<!-- Make sure that you can tick the boxes below. -->

* [x] The PR contains a test case for the changes or the changes are
already covered by an existing test case.
* [x] The documentation (`docs/`) is updated to reflect the changes or
this is not necessary (e.g. if the change does neither modify the
language nor the behavior or functionalities of Snakemake).

---------

Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>
Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

setup.cfg
@@ -31,7 +31,7 @@ classifiers =
 zip_safe = False
 include_package_data = False
 packages = find:
-python_requires = >=3.9
+python_requires = >=3.7
 install_requires =
     appdirs
     configargparse

snakemake/__init__.py
@@ -4,9 +4,13 @@ __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
 
 import sys
+from snakemake.common import MIN_PY_VERSION
 
-if sys.version_info < (3, 9):
-    raise ValueError("Snakemake requires at least Python 3.9.")
+if sys.version_info < MIN_PY_VERSION:
+    raise ValueError(
+        f"Snakemake requires at least Python {MIN_PY_VERSION}. Please ensure to execute it in a compatible Python environment.",
+        file=sys.stderr,
+    )
 
 import os
 import glob

snakemake/executors/__init__.py
@@ -20,6 +20,7 @@ import tempfile
 from functools import partial
 from collections import namedtuple
 import base64
+from typing import List
 import uuid
 import re
 import math
@@ -142,7 +143,7 @@ class AbstractExecutor(ABC):
 
     def run_jobs(
         self,
-        jobs: list[ExecutorJobInterface],
+        jobs: List[ExecutorJobInterface],
         callback=None,
         submit_callback=None,
         error_callback=None,

snakemake/interfaces.py
@@ -5,14 +5,8 @@ __license__ = "MIT"
 
 import sys
 
-# The typing API used here requires Python 3.9 or later and fails with unintuitive errors otherwise.
-if sys.version_info < (3, 9):
-    raise ValueError(
-        "Snakemake executor interface definition requires at least Python 3.9."
-    )
-
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, List, Dict
 
 
 class ExecutorJobInterface(ABC):
@@ -278,7 +272,7 @@ class WorkflowExecutorInterface(ABC):
 
     @property
     @abstractmethod
-    def rerun_triggers(self) -> Optional[list[str]]:
+    def rerun_triggers(self) -> Optional[List[str]]:
         ...
 
     @property
@@ -328,12 +322,12 @@ class WorkflowExecutorInterface(ABC):
 
     @property
     @abstractmethod
-    def overwrite_threads(self) -> dict[str, int]:
+    def overwrite_threads(self) -> Dict[str, int]:
         ...
 
     @property
     @abstractmethod
-    def overwrite_scatter(self) -> dict[str, int]:
+    def overwrite_scatter(self) -> Dict[str, int]:
         ...
 
     @property

test-environment.yml
@@ -2,7 +2,7 @@ channels:
   - conda-forge
   - bioconda
 dependencies:
-  - python >=3.9
+  - python >=3.7
   - yte
   - packaging
   - stopit

tests/test_conda_python_3_7_script/Snakefile
@@ -1,0 +1,7 @@
+rule random_python_conda_script:
+	output:
+		"version.txt"
+	conda:
+		"test_python_env.yaml"
+	script:
+		"test_script.py"

tests/test_conda_python_3_7_script/test_python_env.yaml
@@ -1,0 +1,9 @@
+channels:
+  - conda-forge
+  - defaults
+dependencies:
+  - python =3.7.12
+  # add a dependency that is not used by Snakemake itself,
+  # to simulate the case where the Python script needs 3.7 and
+  # that dependency
+  - pillow =9.2

tests/test_conda_python_3_7_script/test_script.py
@@ -1,0 +1,5 @@
+import platform
+import PIL
+
+with open('version.txt', 'w') as f:
+    f.write(platform.python_version())

tests/tests.py
@@ -2023,6 +2023,10 @@ def test_conda_python_script():
     run(dpath("test_conda_python_script"), use_conda=True)
 
 
+def test_conda_python_3_7_script():
+    run(dpath("test_conda_python_3_7_script"), use_conda=True)
+
+
 def test_prebuilt_conda_script():
     sp.run("conda env create -f tests/test_prebuilt_conda_script/env.yaml", shell=True)
     run(dpath("test_prebuilt_conda_script"), use_conda=True)