88d96367bdc1eb43cff997474b56a790ecf4f31c
Nils Giordano <nils.giordano@univ-nantes.fr>
2023-08-22 17:31:44 +0200

    fix #1938 DAG is now properly displayed in report layout of the workflow

--- snakemake/report/rulegraph_spec.py
+++ snakemake/report/rulegraph_spec.py
@@ -1,64 +1,70 @@
 from functools import partial
+from collections import defaultdict
 
 
 def rulegraph_spec(dag):
-    # get toposorting, and keep only one job of each rule per level
-    representatives = dict()
+    # Graphviz necessary to generate the layout
+    try:
+        import networkx as nx
+        from networkx.drawing.nx_agraph import graphviz_layout
+        from networkx.readwrite import json_graph
+    except ImportError as e:
+        raise WorkflowError(
+            "Python packages networkx and pygraphviz must be "
+            "installed to create reports.",
+            e,
+        )
 
-    toposorted = [
-        get_representatives(level, representatives) for level in dag.toposorted()
-    ]
+    # Using sets make sure you have a single representative
+    graph = defaultdict(set)
+    for job in dag.jobs:
+        graph[job.rule].update(dep.rule for dep in dag.dependencies[job])
 
-    jobs = [job for level in toposorted for job in level]
+    # Generate networkx directed graph, add nodes and edges
+    g = nx.DiGraph()
+    g.add_nodes_from([rule.name for rule in graph.keys()])
+    # For each job, add one edge per dependency
+    for rule, deps in graph.items():
+        target = rule.name
+        for dep in deps:
+            source = dep.name
+            g.add_edge(source, target)
 
-    nodes = [
-        {"rule": job.rule.name, "fx": 10, "fy": i * 50} for i, job in enumerate(jobs)
-    ]
-    idx = {job: i for i, job in enumerate(jobs)}
+    # Generate layout
+    pos = graphviz_layout(g, prog="dot", args="-Grankdir=BT")
+    xmax = max(x for x, y in pos.values()) + 100  # add offset to account for labels
+    ymax = max(y for x, y in pos.values())
 
-    xmax = 100
-    ymax = max(node["fy"] for node in nodes)
+    # Encode nodes for VEGA template
+    def encode_node(node):
+        x, y = pos[node]
+        return {"rule": node, "fx": x, "fy": y}
 
-    _get_links = partial(get_links, jobs, dag, idx, nodes, representatives)
+    nodes = list(map(encode_node, g.nodes))
+    nodeids = {node["rule"]: i for i, node in enumerate(nodes)}
 
-    return (
+    # Encode links and links_direct for VEGA template
+    def encode_link(edge):
+        return {
+            "target": nodeids[edge[0]],
+            "source": nodeids[edge[1]],
+            "targetrule": edge[0],
+            "sourcerule": edge[1],
+            "value": 1,
+        }
+
+    links = list(map(encode_link, g.edges))
+    # I’m not sure why links_direct is necessary here, keeping it for compatibility
+    links_direct = list(map(encode_link, g.edges))
+
+    spec = (
         {
             "nodes": nodes,
-            "links": list(_get_links(direct=False)),
-            "links_direct": list(_get_links(direct=True)),
+            "links": links,
+            "links_direct": links_direct,
         },
         xmax,
         ymax,
     )
 
-
-def get_representatives(level: list, representatives: dict):
-    unique = dict()
-    for job in level:
-        if job.rule.name in unique:
-            representatives[job] = unique[job.rule.name]
-        else:
-            representatives[job] = job
-            unique[job.rule.name] = job
-    return sorted(unique.values(), key=lambda job: job.rule.name)
-
-
-def get_links(jobs, dag, idx, nodes, representatives, direct: bool):
-    for u in jobs:
-        for v in dag.dependencies[u]:
-            target = idx[u]
-            source = idx[representatives[v]]
-            if target - source == 1:
-                if not direct:
-                    continue
-            else:
-                if direct:
-                    continue
-
-            yield {
-                "target": target,
-                "source": source,
-                "sourcerule": nodes[source]["rule"],
-                "targetrule": nodes[target]["rule"],
-                "value": 1,
-            }
+    return spec
