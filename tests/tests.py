83a253477d9cb9b3b7b7841a782c542975d96ea1
Felix Mölder <felix.moelder@uni-due.de>
2023-09-11 11:48:34 +0200

    add test

--- tests/tests.py
+++ tests/tests.py
@@ -2104,3 +2104,7 @@ def test_module_wildcard_constraints():
 @skip_on_windows
 def test_config_yte():
     run(dpath("test_config_yte"))
+
+
+def test_load_metawrapper():
+    run(dpath("test_load_metawrapper"), dryrun=True)
