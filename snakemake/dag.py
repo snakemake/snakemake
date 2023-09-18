b2a60d5674c84f58c6e48af5d675ce4690a1d625
Johannes Köster <johannes.koester@tu-dortmund.de>
2023-09-18 15:02:55 +0200

    fix: ensure that targetjob is always forced. This fixes a bug causing run-directive rules to not being executed even when enforced via e.g. -R. (#2448)
    ### Description
    
    <!--Add a description of your PR here-->
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [ ] The PR contains a test case for the changes or the changes are
    already covered by an existing test case.
    * [ ] The documentation (`docs/`) is updated to reflect the changes or
    this is not necessary (e.g. if the change does neither modify the
    language nor the behavior or functionalities of Snakemake).

--- snakemake/dag.py
+++ snakemake/dag.py
@@ -177,6 +177,7 @@ class DAG(DAGExecutorInterface):
                 create_inventory=True,
             )
             self.targetjobs.add(job)
+            self.forcefiles.update(job.output)
 
         self.cleanup()
 
