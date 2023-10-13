8332d2d28af0900724f7a56aa0e394a99f48d9e0
Johannes Koester <johannes.koester@uni-due.de>
2023-10-13 15:52:50 +0200

    fix test suite

--- snakemake/common/tests/__init__.py
+++ snakemake/common/tests/__init__.py
@@ -2,10 +2,12 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 import shutil
 from typing import List, Mapping, Optional
+import uuid
 
 import pytest
 from snakemake import api, settings
 
+from snakemake_interface_common.utils import lazy_property
 from snakemake_interface_common.plugin_registry.plugin import TaggedSettings
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 from snakemake_interface_executor_plugins.registry import ExecutorPluginRegistry
@@ -160,30 +162,23 @@ class TestWorkflowsLocalStorageBase(TestWorkflowsBase):
 
 
 class TestWorkflowsMinioPlayStorageBase(TestWorkflowsBase):
-    def __init__(self):
-        import uuid
-        from snakemake_storage_plugin_s3 import StorageProviderSettings
-
-        endpoint_url = "https://play.minio.io:9000"
-        access_key = "Q3AM3UQ867SPQQA43P2F"
-        secret_key = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
-        self._bucket = f"snakemake-{uuid.uuid4().hex}"
-
-        self._storage_provider_settings = StorageProviderSettings(
-            endpoint_url=endpoint_url,
-            access_key=access_key,
-            secret_key=secret_key,
-        )
-
     def get_default_storage_provider(self) -> Optional[str]:
         return "s3"
 
     def get_default_storage_prefix(self) -> Optional[str]:
-        return f"s3://{self._bucket}"
+        return f"s3://{self.bucket}"
 
     def get_default_storage_provider_settings(
         self,
     ) -> Optional[Mapping[str, TaggedSettings]]:
+        from snakemake_storage_plugin_s3 import StorageProviderSettings
+
+        self._storage_provider_settings = StorageProviderSettings(
+            endpoint_url=self.endpoint_url,
+            access_key=self.access_key,
+            secret_key=self.secret_key,
+        )
+
         tagged_settings = TaggedSettings()
         tagged_settings.register_settings(self._storage_provider_settings)
         return {"s3": tagged_settings}
@@ -194,11 +189,27 @@ class TestWorkflowsMinioPlayStorageBase(TestWorkflowsBase):
         # clean up using boto3
         s3c = boto3.resource(
             "s3",
-            endpoint_url=self._storage_provider_settings.endpoint_url,
-            aws_access_key_id=self._storage_provider_settings.access_key,
-            aws_secret_access_key=self._storage_provider_settings.secret_key,
+            endpoint_url=self.endpoint_url,
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key,
         )
         try:
-            s3c.Bucket(self._bucket).delete()
+            s3c.Bucket(self.bucket).delete()
         except Exception:
             pass
+
+    @lazy_property
+    def bucket(self):
+        return f"snakemake-{uuid.uuid4().hex}"
+
+    @property
+    def enpoint_url(self):
+        return "https://play.minio.io:9000"
+
+    @property
+    def access_key(self):
+        return "Q3AM3UQ867SPQQA43P2F"
+
+    @property
+    def secret_key(self):
+        return "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
