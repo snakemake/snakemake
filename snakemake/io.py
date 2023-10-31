16fbb2fb5d45667842ee7fdfd1c8e1bbfd540181
Morten Enemark Lund <melund@gmail.com>
2023-10-31 20:25:24 +0100

    Move windows slash check into IOFile constructor

--- snakemake/io.py
+++ snakemake/io.py
@@ -200,6 +200,8 @@ class IOCache(IOCacheStorageInterface):
 
 def IOFile(file, rule=None):
     assert rule is not None
+    if ON_WINDOWS and isinstance(file, str):
+        file = file.replace("\\", "/")
     f = _IOFile(file)
     f.rule = rule
     return f
@@ -734,8 +736,6 @@ class _IOFile(str, AnnotatedStringInterface):
                     rule=self.rule,
                 )
             local_path_str = str(storage_object.local_path())
-            if ON_WINDOWS:
-                local_path_str = local_path_str.replace("\\", "/")
             file_with_wildcards_applied = IOFile(local_path_str, rule=self.rule)
             file_with_wildcards_applied.clone_flags(self, skip_storage_object=True)
             file_with_wildcards_applied.flags["storage_object"] = storage_object
