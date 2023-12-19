8d5e943152e1e8451b07e165bc04264f58467749
Johannes Koester <johannes.koester@uni-due.de>
2023-12-19 18:28:32 +0100

    fix path

--- apidocs/conf.py
+++ apidocs/conf.py
@@ -57,7 +57,7 @@ source_suffix = [".rst", ".md"]
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = "apidocs/index"
+master_doc = "index"
 
 # General information about the project.
 project = "Snakemake-API"
