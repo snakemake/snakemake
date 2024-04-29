28bdc56e6594306c811db2301bb6649a7555b78a
Jesse Wallace <jesse.rudolf.wallace@gmail.com>
2024-04-29 10:07:05 +1000

    Correct formatting

--- snakemake/io.py
+++ snakemake/io.py
@@ -329,7 +329,14 @@ class _IOFile(str, AnnotatedStringInterface):
         """
         if self.is_storage and not self.exists_local():
             async_run(self.retrieve_from_storage())
-        f = open(self, mode=mode, buffering=buffering, encoding=encoding, errors=errors, newline=newline)
+        f = open(
+            self,
+            mode=mode,
+            buffering=buffering,
+            encoding=encoding,
+            errors=errors,
+            newline=newline,
+        )
         try:
             yield f
         finally:
