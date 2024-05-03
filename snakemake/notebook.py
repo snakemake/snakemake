d4e177ba9f288d04b67af9c801d5b292bb640cb6
Johannes Koester <johannes.koester@uni-due.de>
2024-05-03 09:10:11 +0200

    fix

--- snakemake/notebook.py
+++ snakemake/notebook.py
@@ -252,6 +252,7 @@ def notebook(
     edit,
     sourcecache_path,
     runtime_sourcecache_path,
+    shell_exec,
 ):
     """
     Load a script from the given basedir + path and execute it.
@@ -325,6 +326,7 @@ def notebook(
         cleanup_scripts,
         shadow_dir,
         is_local,
+        shell_exec,
     )
 
     if edit is None:
