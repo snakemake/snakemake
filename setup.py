dbff081f706a2cda77bde10e1df1818fec314cbd
Johannes Koester <johannes.koester@uni-due.de>
2024-08-28 14:43:44 +0200

    fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import sys
 
 from setuptools import setup
@@ -6,10 +7,15 @@ from setuptools import setup
 # ensure the current directory is on sys.path so versioneer can be imported
 # when pip uses PEP 517/518 build rules.
 # https://github.com/python-versioneer/python-versioneer/issues/193
-sys.path.append(os.path.dirname(__file__))
 
+
+sys.path.append(str(Path(__file__).parent))
 import versioneer  # noqa: E402
-from snakemake.assets import Assets
+
+# import Assets, while avoiding that the rest of snakemake is imported here before
+# setup has been called.
+sys.path.append(str(Path(__file__).parent / "snakemake"))
+from assets import Assets
 
 # download online assets
 Assets.deploy()
