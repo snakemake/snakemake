d82453b7e54321c817c6516fca9d9e16c03c3977
Johannes Köster <johannes.koester@tu-dortmund.de>
2024-09-04 14:06:35 +0200

    fix: fix issues with misinterpretation of max-jobs-per-timespan and max-jobs-per-seconds (#3067)
    fixes #3042, fixes #3019
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] The PR contains a test case for the changes or the changes are
    already covered by an existing test case.
    * [x] The documentation (`docs/`) is updated to reflect the changes or
    this is not necessary (e.g. if the change does neither modify the
    language nor the behavior or functionalities of Snakemake).
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Improvements**
    	- Enhanced job scheduling logic for better clarity and maintainability.
    	- Introduced a new property for easier access to job timespan values.
    	- Added debug logging to improve observability of job scheduling state.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- snakemake/scheduler.py
+++ snakemake/scheduler.py
@@ -247,10 +247,9 @@ class JobScheduler(JobSchedulerExecutorInterface):
                     if self.workflow.dag.has_unfinished_queue_input_jobs():
                         logger.info("Waiting for queue input...")
                         # schedule a reevaluation in 10 seconds
-                        threading.Timer(
-                            self.workflow.execution_settings.queue_input_wait_time,
-                            lambda: self._open_jobs.release(),
-                        ).start()
+                        self._schedule_reevalutation(
+                            self.workflow.execution_settings.queue_input_wait_time
+                        )
                     continue
 
                 # select jobs by solving knapsack problem (omit with dryrun)
@@ -314,12 +313,22 @@ class JobScheduler(JobSchedulerExecutorInterface):
                         self.run(runjobs)
                 elif not self.dryrun:
                     logger.info("Waiting for more resources.")
+                    if self.job_rate_limiter is not None:
+                        # need to reevaluate because after the timespan we can
+                        # schedule more jobs again
+                        self._schedule_reevalutation(self.job_rate_limiter.timespan)
         except (KeyboardInterrupt, SystemExit):
             logger.info(
                 "Terminating processes on user request, this might take some time."
             )
             self._executor.cancel()
             return False
+
+    def _schedule_reevalutation(self, delay: int) -> None:
+        threading.Timer(
+            delay,
+            lambda: self._open_jobs.release(),
+        ).start()
 
     def _finish_jobs(self):
         # must be called from within lock
@@ -815,6 +824,10 @@ class JobRateLimiter:
         self._limit: MaxJobsPerTimespan = limit
         self._jobs = deque()
 
+    @property
+    def timespan(self) -> int:
+        return self._limit.timespan
+
     def register_jobs(self, n_jobs: int):
         currtime = time.time()
         self._jobs.extend(repeat(currtime, n_jobs))
@@ -825,4 +838,8 @@ class JobRateLimiter:
         # remove the first index elements from the deque
         for _ in range(index):
             self._jobs.popleft()
-        return max(self._limit.max_jobs - len(self._jobs), 0)
+        n_free = max(self._limit.max_jobs - len(self._jobs), 0)
+        logger.debug(
+            f"Free jobs: {n_free}, jobs in timespan: {len(self._jobs)}, limit: {self._limit.max_jobs}, timespan: {self._limit.timespan}"
+        )
+        return n_free
