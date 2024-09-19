snakemake/snakemake

commit 6f582f180f5df9292e005623f093a3a0b2e597a7

feat: Support conda environment definitions to be passed as function pointers, similar to input, params, and resources (#1300)
* Format conda env path with job parameters (there's probably a better way?)

* Use snakemake.utils.format to format conda env path with wildcards and parameters

* Find better code location to insert job parameters into conda env path

* Nothing to see here, move along

* Use str.format instead of snakemake.utils.format (we do not want to use scoped variables)

* Format code

* Apply parameters to conda env in _IOFile.apply_params

* Allow function as conda parameter

* Initial (wrong) draft for an improved conda expansion

* Implement suggestions

* Fix argument passing

* Make params and input paramers of expand_conda_env optional

* Fix logic in expand_conda_env

* Remove old code

* Update snakemake/rules.py

Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

* Return early if conda_env is None

* Document changes

* Fix is_local_file import

* Fix wildcard application for strings in conda expansion

* Improve logic

* Once more

* Remove unneeded check

* Improve SourceFile handling

* clean up and refine conda env expansion. Make sure that always a SourceFile is passed.

* typo

* Fix archival

* Fix --containerize

* Fix for --all-temp run

* Document report limitations

* Add test

* Run test_conda_function with single core

* Fix test

Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

snakemake/rules.py
@@ -28,8 +28,6 @@ from snakemake.io import (
     update_wildcard_constraints,
     flag,
     get_flag_value,
-)
-from snakemake.io import (
     expand,
     InputFiles,
     OutputFiles,
@@ -38,10 +36,9 @@ from snakemake.io import (
     Log,
     Resources,
     strip_wildcard_constraints,
-)
-from snakemake.io import (
     apply_wildcards,
     is_flagged,
+    flag,
     not_iterable,
     is_callable,
     DYNAMIC_FILL,
@@ -64,7 +61,6 @@ from snakemake.common import (
     lazy_property,
     TBDString,
 )
-import snakemake.io
 
 
 class Rule:
@@ -371,16 +367,7 @@ class Rule:
 
     @conda_env.setter
     def conda_env(self, conda_env):
-        from snakemake.deployment.conda import (
-            is_conda_env_file,
-            CondaEnvFileSpec,
-            CondaEnvNameSpec,
-        )
-
-        if is_conda_env_file(conda_env):
-            self._conda_env = CondaEnvFileSpec(conda_env, rule=self)
-        else:
-            self._conda_env = CondaEnvNameSpec(conda_env)
+        self._conda_env = conda_env
 
     @property
     def container_img(self):
@@ -523,6 +510,7 @@ class Rule:
         inoutput -- a Namedlist of either input or output items
         name     -- an optional name for the item
         """
+
         inoutput = self.output if output else self.input
 
         # Check to see if the item is a path, if so, just make it a string
@@ -551,8 +539,8 @@ class Rule:
             # Check to see that all flags are valid
             # Note that "remote", "dynamic", and "expand" are valid for both inputs and outputs.
             if isinstance(item, AnnotatedString):
-                for flag in item.flags:
-                    if not output and flag in [
+                for item_flag in item.flags:
+                    if not output and item_flag in [
                         "protected",
                         "temp",
                         "temporary",
@@ -564,13 +552,13 @@ class Rule:
                     ]:
                         logger.warning(
                             "The flag '{}' used in rule {} is only valid for outputs, not inputs.".format(
-                                flag, self
+                                item_flag, self
                             )
                         )
-                    if output and flag in ["ancient"]:
+                    if output and item_flag in ["ancient"]:
                         logger.warning(
                             "The flag '{}' used in rule {} is only valid for inputs, not outputs.".format(
-                                flag, self
+                                item_flag, self
                             )
                         )
 
@@ -592,7 +580,7 @@ class Rule:
 
             if self.workflow.all_temp and output:
                 # mark as temp if all output files shall be marked as temp
-                item = snakemake.io.flag(item, "temp")
+                item = flag(item, "temp")
 
             # record rule if this is an output file output
             _item = IOFile(item, rule=self)
@@ -1125,23 +1113,41 @@ class Rule:
         else:
             return self.group
 
-    def expand_conda_env(self, wildcards):
-        try:
-            conda_env = (
-                self.conda_env.apply_wildcards(wildcards, self)
-                if self.conda_env
-                else None
-            )
-        except WildcardError as e:
-            raise WildcardError(
-                "Wildcards in conda environment file cannot be "
-                "determined from output files:",
-                str(e),
-                rule=self,
+    def expand_conda_env(self, wildcards, params=None, input=None):
+        from snakemake.common import is_local_file
+        from snakemake.sourcecache import SourceFile, infer_source_file
+        from snakemake.deployment.conda import (
+            is_conda_env_file,
+            CondaEnvFileSpec,
+            CondaEnvNameSpec,
+        )
+
+        conda_env = self._conda_env
+        if callable(conda_env):
+            conda_env, _ = self.apply_input_function(
+                conda_env, wildcards=wildcards, params=params, input=input
             )
 
-        if conda_env is not None:
-            conda_env.check()
+        if conda_env is None:
+            return None
+
+        if is_conda_env_file(conda_env):
+            if not isinstance(conda_env, SourceFile):
+                if is_local_file(conda_env) and not os.path.isabs(conda_env):
+                    # Conda env file paths are considered to be relative to the directory of the Snakefile
+                    # hence we adjust the path accordingly.
+                    # This is not necessary in case of receiving a SourceFile.
+                    conda_env = self.basedir.join(conda_env)
+                else:
+                    # infer source file from unmodified uri or path
+                    conda_env = infer_source_file(conda_env)
+
+            conda_env = CondaEnvFileSpec(conda_env, rule=self)
+        else:
+            conda_env = CondaEnvNameSpec(conda_env)
+
+        conda_env = conda_env.apply_wildcards(wildcards, self)
+        conda_env.check()
 
         return conda_env
 

snakemake/__init__.py
@@ -621,8 +621,6 @@ def snakemake(
         if not print_compilation:
             if lint:
                 success = not workflow.lint(json=lint == "json")
-            elif containerize:
-                workflow.containerize()
             elif listrules:
                 workflow.list_rules()
             elif list_target_rules:
@@ -808,6 +806,7 @@ def snakemake(
                     export_cwl=export_cwl,
                     batch=batch,
                     keepincomplete=keep_incomplete,
+                    containerize=containerize,
                 )
 
     except BrokenPipeError:
@@ -1078,7 +1077,7 @@ def get_argument_parser(profile=None):
                         line options in YAML format. For example,
                         '--cluster qsub' becomes 'cluster: qsub' in the YAML
                         file. Profiles can be obtained from
-                        https://github.com/snakemake-profiles. 
+                        https://github.com/snakemake-profiles.
                         The profile can also be set via the environment variable $SNAKEMAKE_PROFILE.
                         """.format(
             dirs.site_config_dir, dirs.user_config_dir

snakemake/cwl.py
@@ -228,7 +228,7 @@ def dag_to_cwl(dag):
             "sources": {
                 "type": "File[]",
                 "default": [
-                    {"class": "File", "location": f} for f in dag.workflow.get_sources()
+                    {"class": "File", "location": f} for f in dag.get_sources()
                 ],
             },
             "cores": {

snakemake/dag.py
@@ -16,6 +16,7 @@ from functools import partial
 from pathlib import Path
 import uuid
 import math
+import subprocess
 
 from snakemake.io import (
     PeriodicityDetector,
@@ -34,10 +35,14 @@ from snakemake.exceptions import PeriodicWildcardError
 from snakemake.exceptions import RemoteFileException, WorkflowError, ChildIOException
 from snakemake.exceptions import InputFunctionException
 from snakemake.logging import logger
-from snakemake.common import DYNAMIC_FILL, ON_WINDOWS, group_into_chunks
+from snakemake.common import DYNAMIC_FILL, ON_WINDOWS, group_into_chunks, is_local_file
 from snakemake.deployment import conda, singularity
 from snakemake.output_index import OutputIndex
 from snakemake import workflow
+from snakemake.sourcecache import (
+    LocalSourceFile,
+    SourceFile,
+)
 
 
 PotentialDependency = namedtuple("PotentialDependency", ["file", "jobs", "known"])
@@ -2250,7 +2255,7 @@ class DAG:
                     "Archiving snakefiles, scripts and files under "
                     "version control..."
                 )
-                for f in self.workflow.get_sources():
+                for f in self.get_sources():
                     add(f)
 
                 logger.info("Archiving external input files...")
@@ -2417,6 +2422,94 @@ class DAG:
                 if not job.is_group() and (include_needrun or not self.needrun(job)):
                     changed.extend(list(job.outputs_older_than_script_or_notebook()))
         return changed
+
+    def warn_about_changes(self, quiet=False):
+        if not quiet:
+            for change_type in ["code", "input", "params"]:
+                changed = self.get_outputs_with_changes(
+                    change_type, include_needrun=False
+                )
+                if changed:
+                    rerun_trigger = ""
+                    if not ON_WINDOWS:
+                        rerun_trigger = f"\n    To trigger a re-run, use 'snakemake -R $(snakemake --list-{change_type}-changes)'."
+                    logger.warning(
+                        f"The {change_type} used to generate one or several output files has changed:\n"
+                        f"    To inspect which output files have changes, run 'snakemake --list-{change_type}-changes'."
+                        f"{rerun_trigger}"
+                    )
+
+    def get_sources(self):
+        files = set()
+
+        def local_path(f):
+            if not isinstance(f, SourceFile) and is_local_file(f):
+                return f
+            if isinstance(f, LocalSourceFile):
+                return f.get_path_or_uri()
+
+        def norm_rule_relpath(f, rule):
+            if not os.path.isabs(f):
+                f = os.path.join(rule.basedir, f)
+            return os.path.relpath(f)
+
+        # get registered sources
+        for f in self.workflow.included:
+            f = local_path(f)
+            if f:
+                try:
+                    f = os.path.relpath(f)
+                except ValueError:
+                    if ON_WINDOWS:
+                        pass  # relpath doesn't work on win if files are on different drive
+                    else:
+                        raise
+                files.add(f)
+        for rule in self.workflow.rules:
+            script_path = rule.script or rule.notebook
+            if script_path:
+                script_path = norm_rule_relpath(script_path, rule)
+                files.add(script_path)
+                script_dir = os.path.dirname(script_path)
+                files.update(
+                    os.path.join(dirpath, f)
+                    for dirpath, _, files in os.walk(script_dir)
+                    for f in files
+                )
+
+        for job in self.jobs:
+            if job.conda_env_spec and job.conda_env_spec.is_file:
+                f = local_path(job.conda_env_spec.file)
+                if f:
+                    # url points to a local env file
+                    env_path = norm_rule_relpath(f, job.rule)
+                    files.add(env_path)
+
+        for f in self.workflow.configfiles:
+            files.add(f)
+
+        # get git-managed files
+        # TODO allow a manifest file as alternative
+        try:
+            out = subprocess.check_output(
+                ["git", "ls-files", "--recurse-submodules", "."], stderr=subprocess.PIPE
+            )
+            for f in out.decode().split("\n"):
+                if f:
+                    files.add(os.path.relpath(f))
+        except subprocess.CalledProcessError as e:
+            if "fatal: not a git repository" in e.stderr.decode().lower():
+                logger.warning(
+                    "Unable to retrieve additional files from git. "
+                    "This is not a git repository."
+                )
+            else:
+                raise WorkflowError(
+                    "Error executing git (Snakemake requires git to be installed for "
+                    "remote execution without shared filesystem):\n" + e.stderr.decode()
+                )
+
+        return files
 
     def __str__(self):
         return self.dot()

snakemake/deployment/containerize.py
@@ -13,11 +13,11 @@ from snakemake.sourcecache import LocalSourceFile
 CONDA_ENV_PATH = "/conda-envs"
 
 
-def containerize(workflow):
+def containerize(workflow, dag):
     if any(
-        rule.conda_env.contains_wildcard
-        for rule in workflow.rules
-        if rule.conda_env is not None
+        job.conda_env_spec.contains_wildcard
+        for job in dag.jobs
+        if job.conda_env_spec is not None
     ):
         raise WorkflowError(
             "Containerization of conda based workflows is not allowed if any conda env definition contains a wildcard."
@@ -31,9 +31,9 @@ def containerize(workflow):
 
     envs = sorted(
         set(
-            rule.conda_env.get_conda_env(workflow, env_dir=CONDA_ENV_PATH)
-            for rule in workflow.rules
-            if rule.conda_env is not None
+            job.conda_env_spec.get_conda_env(workflow, env_dir=CONDA_ENV_PATH)
+            for job in dag.jobs
+            if job.conda_env_spec is not None
         ),
         key=relfile,
     )

snakemake/executors/__init__.py
@@ -1684,7 +1684,7 @@ class KubernetesExecutor(ClusterExecutor):
         secret.metadata.name = self.run_namespace
         secret.type = "Opaque"
         secret.data = {}
-        for i, f in enumerate(self.workflow.get_sources()):
+        for i, f in enumerate(self.dag.get_sources()):
             if f.startswith(".."):
                 logger.warning(
                     "Ignoring source file {}. Only files relative "
@@ -2083,7 +2083,7 @@ class TibannaExecutor(ClusterExecutor):
     ):
         self.workflow = workflow
         self.workflow_sources = []
-        for wfs in workflow.get_sources():
+        for wfs in dag.get_sources():
             if os.path.isdir(wfs):
                 for (dirpath, dirnames, filenames) in os.walk(wfs):
                     self.workflow_sources.extend(

snakemake/executors/ga4gh_tes.py
@@ -234,7 +234,7 @@ class TaskExecutionServiceExecutor(ClusterExecutor):
         inputs = []
 
         # add workflow sources to inputs
-        for src in self.workflow.get_sources():
+        for src in self.dag.get_sources():
             # exclude missing, hidden, empty and build files
             if (
                 not os.path.exists(src)

snakemake/executors/google_lifesciences.py
@@ -636,7 +636,7 @@ class GoogleLifeSciencesExecutor(ClusterExecutor):
         """
         self.workflow_sources = []
 
-        for wfs in self.workflow.get_sources():
+        for wfs in self.dag.get_sources():
             if os.path.isdir(wfs):
                 for (dirpath, dirnames, filenames) in os.walk(wfs):
                     self.workflow_sources.extend(

commit 8d34b85ad9795604488d7d2f5bdc121e7d88fee3

fix: cache conda envs to fix performance regression introduced in https://github.com/snakemake/snakemake/pull/1300

snakemake/rules.py
@@ -25,6 +25,7 @@ from snakemake.io import (
     _IOFile,
     Namedlist,
     AnnotatedString,
+    contains_wildcard,
     contains_wildcard_constraints,
     update_wildcard_constraints,
     flag,
@@ -66,6 +67,9 @@ from snakemake_interface_common.utils import not_iterable, lazy_property
 from snakemake_interface_common.rules import RuleInterface
 
 
+_NOT_CACHED = object()
+
+
 class Rule(RuleInterface):
     def __init__(self, name, workflow, lineno=None, snakefile=None):
         """
@@ -92,6 +96,7 @@ class Rule(RuleInterface):
         self._log = Log()
         self._benchmark = None
         self._conda_env = None
+        self._expanded_conda_env = _NOT_CACHED
         self._container_img = None
         self.is_containerized = False
         self.env_modules = None
@@ -1061,6 +1066,9 @@ class Rule(RuleInterface):
             return self.group
 
     def expand_conda_env(self, wildcards, params=None, input=None):
+        if self._expanded_conda_env is not _NOT_CACHED:
+            return self._expanded_conda_env
+
         from snakemake.common import is_local_file
         from snakemake.sourcecache import SourceFile, infer_source_file
         from snakemake.deployment.conda import (
@@ -1070,12 +1078,18 @@ class Rule(RuleInterface):
         )
 
         conda_env = self._conda_env
-        if callable(conda_env):
-            conda_env, _ = self.apply_input_function(
-                conda_env, wildcards=wildcards, params=params, input=input
-            )
-
-        if conda_env is None:
+        if conda_env is not None:
+            if not callable(conda_env):
+                cacheable = not contains_wildcard(conda_env)
+            else:
+                conda_env, _ = self.apply_input_function(
+                    conda_env, wildcards=wildcards, params=params, input=input
+                )
+                cacheable = False
+                if conda_env is None:
+                    return None
+        else:
+            self._expanded_conda_env = None
             return None
 
         if is_conda_env_file(conda_env):
@@ -1095,6 +1109,9 @@ class Rule(RuleInterface):
 
         conda_env = conda_env.apply_wildcards(wildcards, self)
         conda_env.check()
+
+        if cacheable:
+            self._expanded_conda_env = conda_env
 
         return conda_env
 