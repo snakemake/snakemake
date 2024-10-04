53704f867ca6115f610bc7d289aa87b0e2dc240f
Johannes Koester <johannes.koester@uni-due.de>
2024-10-04 17:00:02 +0200

    remove dbg code

--- snakemake/jobs.py
+++ snakemake/jobs.py
@@ -1245,7 +1245,6 @@ class GroupJob(AbstractJob, GroupJobExecutorInterface):
     ]
 
     def __init__(self, id, jobs, global_resources):
-        breakpoint()
         self.groupid = id
         self._jobs = jobs
         self.global_resources = global_resources
