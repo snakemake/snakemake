04fb97ff8b10fafdaf668368cb29e468d5bc07a4
Johannes Köster <johannes.koester@tu-dortmund.de>
2024-10-11 14:46:36 +0200

    fix: better error handling in evaluate function (#3129)
    <!--Add a description of your PR here-->
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] The PR contains a test case for the changes or the changes are
    already covered by an existing test case.
    * [x] The documentation (`docs/`) is updated to reflect the changes or
    this is not necessary (e.g. if the change does neither modify the
    language nor the behavior or functionalities of Snakemake).
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Improved error handling in the evaluation of expressions with
    wildcards, providing clearer error messages when exceptions occur.
    
    - **Chores**
    	- No changes to the signatures of exported or public entities.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- snakemake/ioutils.py
+++ snakemake/ioutils.py
@@ -235,9 +235,14 @@ def evaluate(expr: str):
     {wildcardname} with the wildcard value represented as a string."""
 
     def inner(wildcards):
-        return eval(
-            expr.format(**{w: repr(v) for w, v in wildcards.items()}), globals()
-        )
+        formatted = expr.format(**{w: repr(v) for w, v in wildcards.items()})
+        try:
+            return eval(formatted, globals())
+        except Exception as e:
+            raise WorkflowError(
+                f"Failed to evaluate expression {expr} with wildcards {wildcards}. Formatted expression: {formatted}",
+                e,
+            )
 
     return inner
 
