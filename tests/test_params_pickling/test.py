6047a27a2fb77386bcf2e75dddd7cc82187601b3
Johannes Köster <johannes.koester@tu-dortmund.de>
2024-10-30 16:52:06 +0100

    Update tests/test_params_pickling/test.py
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- tests/test_params_pickling/test.py
+++ tests/test_params_pickling/test.py
@@ -2,9 +2,9 @@ import numpy as np
 import polars as pl
 import pandas as pd
 
-assert type(snakemake.params.testnp) == np.ndarray
-assert type(snakemake.params.testpl) == pl.DataFrame
-assert type(snakemake.params.testpd) == pd.DataFrame
+assert isinstance(snakemake.params.testnp, np.ndarray)
+assert isinstance(snakemake.params.testpl, pl.DataFrame)
+assert isinstance(snakemake.params.testpd, pd.DataFrame)
 
 snakemake.params.testpd.to_csv(snakemake.output.pd, sep="\t")
 snakemake.params.testpl.write_csv(snakemake.output.pl, separator="\t")
