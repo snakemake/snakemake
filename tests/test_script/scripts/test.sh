fe9dc4e416daeb7d03376a4a7ebaf58ecce329cd
Michael Hall <michael@mbh.sh>
2024-11-06 22:49:43 +1000

    test: add test to bash script to check params are set

--- tests/test_script/scripts/test.sh
+++ tests/test_script/scripts/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+test "${snakemake_params[string]}" = "foo"
+
 echo "The first input file is ${snakemake_input[0]}" > "${snakemake_output[0]}" 2> "${snakemake_log[0]}"
 echo "The named input file is ${snakemake_input[named]}" >> "${snakemake_output[0]}" 2>> "${snakemake_log[0]}"
 echo "The requested number of threads is ${snakemake[threads]}" >> "${snakemake_output[0]}" 2>> "${snakemake_log[0]}"
