55499c01a6ad134d2c999cbd3ebb62fefc162b28
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-13 16:04:07 +0100

    optimized version

--- snakemake/common/prefix_lookup.py
+++ snakemake/common/prefix_lookup.py
@@ -1,27 +1,35 @@
 import bisect
+from collections import defaultdict
 from typing import Sequence, TypeVar, Generator
 
 V = TypeVar("V")
 
 
 class PrefixLookup:
+    """A data structure for efficiently looking up entries by their prefix."""
+
     def __init__(self, entries: Sequence[tuple[str, V]]) -> None:
-        self._entries = sorted(entries, key=lambda x: x[0])
+        grouped = defaultdict(list)
+        for key, value in entries:
+            grouped[key].append(value)
+        self._entries = sorted(grouped.items(), key=lambda x: x[0])
 
     def match(self, key: str) -> set[V]:
+        """Returns a set of all entries which are prefixes of the given key."""
         return set(self.match_iter(key))
 
     def match_iter(self, key: str) -> Generator[V, None, None]:
-        """Returns all entries which are prefixes of the given key.
+        """Yields all entries which are prefixes of the given key.
 
         E.g. if "abc" is the key then "abc", "ab", "a", and "" are considered valid prefixes.
         """
         stop_idx = bisect.bisect_right(self._entries, key, key=lambda x: x[0])
         last_len = float("inf")
         for index in range(stop_idx - 1, -1, -1):
-            k, entry = self._entries[index]
+            k, entries = self._entries[index]
             if key.startswith(k):
-                yield entry
+                for entry in entries:
+                    yield entry
                 last_len = len(k)
             elif len(k) > last_len:
                 # If we find a non-matching prefix shorter than our last match, we can stop
