2d08c6301ed0a7f56c223d54b708cf42597fe4a2
Kyle Johnsen <johnsenkyle13@gmail.com>
2024-12-21 08:31:22 -0500

    fix: problem with spaces in path (#3236)
    Fixes #3235 
    Goes together with
    https://github.com/snakemake/snakemake-interface-executor-plugins/pull/79
    This PR updates tests to catch the bug (should break at least
    `test_path_with_spaces` and `test_with_parentheses` and
    https://github.com/snakemake/snakemake-interface-executor-plugins/pull/79
    actually fixes it
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new rule that generates an output file with a predefined
    string.
    - **Tests**
    	- Added a test case to verify the handling of paths containing spaces.
    - Enhanced temporary directory naming for clarity during test execution.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>
    Co-authored-by: Johannes Koester <johannes.koester@uni-due.de>

--- tests/tests.py
+++ tests/tests.py
@@ -660,6 +660,8 @@ def test_format_wildcards():
 def test_with_parentheses():
     run(dpath("test (with parenthese's)"))
 
+    run(dpath("test_path with spaces"))
+
 
 def test_dup_out_patterns():
     """Duplicate output patterns should emit an error
