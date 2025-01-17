9eff9a3f5a8156bb677cf9b5d57f9face1d6704d
Tim Booth <tim.booth@ed.ac.uk>
2025-01-17 11:16:02 +0000

    Prefix lookup using repeated bisection rather than backward scanning.
    I'm not sure if this will actually ever make things faster in real world
    usage, but it's an elegant algorithm IMO.

--- snakemake/common/prefix_lookup.py
+++ snakemake/common/prefix_lookup.py
@@ -12,7 +12,7 @@ class PrefixLookup:
         grouped = defaultdict(list)
         for key, value in entries:
             grouped[key].append(value)
-        self._entries = sorted(grouped.items(), key=lambda x: x[0])
+        self._entries = sorted(grouped.items())
 
     def match(self, query: str) -> set[V]:
         """Returns a set of all entries which are prefixes of the given key."""
@@ -23,14 +23,32 @@ class PrefixLookup:
 
         E.g. if "abc" is the key then "abc", "ab", "a", and "" are considered valid prefixes.
         """
-        stop_idx = bisect.bisect_right(self._entries, query, key=lambda x: x[0])
-        last_len = float("inf")
-        for index in range(stop_idx - 1, -1, -1):
-            k, entries = self._entries[index]
+        stop_idx = len(self._entries)
+        while stop_idx:
+            stop_idx = bisect.bisect_right( self._entries,
+                                            query,
+                                            hi=stop_idx,
+                                            key=lambda x: x[0])
+
+            k, entries = self._entries[stop_idx-1]
             if query.startswith(k):
                 for entry in entries:
                     yield entry
-                last_len = len(k)
-            elif len(k) > last_len:
-                # If we find a non-matching prefix shorter than our last matched prefix, we can stop.
+
+            if not query:
+                # Prevent infinite loop if '' is first string in list
                 break
+
+            query = self._common_prefix(k, query)
+
+    def _common_prefix(self, s1: str, s2: str) -> str:
+        """Utility function that gives the common prefix of two strings.
+
+           Except, if the strings are identical, returns the string minus one letter.
+           Behaviour is undefined if either string is empty.
+        """
+        for i, (l1, l2) in enumerate(zip(s1, s2)):
+            if l1 != l2:
+                break
+
+        return s1[:i]
