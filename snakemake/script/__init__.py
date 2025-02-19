dbbe6c764c16f7fc9102076dd460767b83be6304
Lucas Frérot <lucas.frerot@sorbonne-universite.fr>
2025-02-19 13:39:29 +0100

    fix: merging strings

--- snakemake/script/__init__.py
+++ snakemake/script/__init__.py
@@ -532,7 +532,9 @@ class BashEncoder:
         """Converts a dictionary to an associative array"""
         s = "( "
         for k, v in d.items():
-            s += f'[{k}]="{v}" '
+            formatted_v = shlex.quote(f"{v}")
+            formatted_k = shlex.quote(f"{k}")
+            s += f'[$(printf "%s" {formatted_k})]="$(printf "%s" {formatted_v})" '
 
         s += ")"
         return s
