9af962c572bd46671ff2ee8d8c10fbe3f4db3b1a
Lucas Frérot <lucas.frerot@sorbonne-universite.fr>
2025-02-19 09:28:09 +0100

    fix: formatting with black

--- snakemake/script/__init__.py
+++ snakemake/script/__init__.py
@@ -471,10 +471,7 @@ class BashEncoder:
         for k, v in d.items():
             formatted_v = shlex.quote(f"{v}")
             formatted_k = shlex.quote(f"{k}")
-            s += (
-                f'[$(printf "%s" {formatted_k})]'
-                f'="$(printf "%s" {formatted_v})" '
-            )
+            s += f'[$(printf "%s" {formatted_k})]' f'="$(printf "%s" {formatted_v})" '
 
         s += ")"
         return s
