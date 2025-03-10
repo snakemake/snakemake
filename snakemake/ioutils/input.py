821f7c0fd78092655e806ddd48e587d5c5824724
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-03-10 19:50:12 +0100

    Add try-catch on pandas import

--- snakemake/ioutils/input.py
+++ snakemake/ioutils/input.py
@@ -10,18 +10,21 @@ def parse_input(infile=None, parser=None, **kwargs):
 
 
 def extract_checksum(infile, **kwargs):
-    import pandas as pd
+    try:
+        import pandas as pd
 
-    fix_file_name = lambda x: x.removeprefix("./")
-    return (
-        pd.read_csv(
-            infile,
-            sep="  ",
-            header=None,
-            engine="python",
-            converters={1: fix_file_name},
+        fix_file_name = lambda x: x.removeprefix("./")
+        return (
+            pd.read_csv(
+                infile,
+                sep="  ",
+                header=None,
+                engine="python",
+                converters={1: fix_file_name},
+            )
+            .set_index(1)
+            .loc[fix_file_name(kwargs.get("file"))]
+            .item()
         )
-        .set_index(1)
-        .loc[fix_file_name(kwargs.get("file"))]
-        .item()
-    )
+    except ImportError:
+        raise WorkflowError("Pandas is required to extract checksum.")
