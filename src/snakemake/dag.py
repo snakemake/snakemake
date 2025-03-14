38aed18794e9c940641117aaabdd234871f70fec
Johannes Köster <johannes.koester@tu-dortmund.de>
2025-03-14 13:57:02 +0100

    Update src/snakemake/dag.py

--- src/snakemake/dag.py
+++ src/snakemake/dag.py
@@ -283,7 +283,7 @@ class DAG(DAGExecutorInterface, DAGReportInterface):
 
         may_exists = await asyncio.gather(*(f.exists() for f in noneedrun_files))
 
-        workflow.checkpoints.created_output = done_output | {
+        self.workflow.checkpoints.created_output = done_output | {
             j for j, e in zip(noneedrun_files, may_exists) if e
         }
 
