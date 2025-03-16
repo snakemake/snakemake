9865cd490e7ace94e2523ff68b5f412c491833bb
tadasbar <tadasbar@gmail.com>
2025-03-16 19:44:11 +0200

    Black reformatting

--- src/snakemake/io.py
+++ src/snakemake/io.py
@@ -1010,14 +1010,10 @@ async def wait_for_files(
 
     async def get_missing(list_parent=False):
         async def eval_file(f):
-            if (
-                is_flagged(f, "nodelocal")
-                and not wait_for_local
-            ):
+            if is_flagged(f, "nodelocal") and not wait_for_local:
                 return None
             if (
-                is_flagged(f, "pipe")
-                or is_flagged(f, "service")
+                is_flagged(f, "pipe") or is_flagged(f, "service")
             ) and ignore_pipe_or_service:
                 return None
             if (
