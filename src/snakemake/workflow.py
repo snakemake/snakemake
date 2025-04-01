c67a52f7299092f25b7bf7897527e8cb93fd1d4c
ASLeonard <alexander.leonard@usys.ethz.ch>
2025-04-01 10:27:45 +0200

    Don't print "Building DAG" for DOT output

--- src/snakemake/workflow.py
+++ src/snakemake/workflow.py
@@ -969,7 +969,7 @@ class Workflow(WorkflowExecutorInterface):
             ignore_incomplete=True,
             lock_warn_only=True,
         )
-        self._build_dag()
+        self._build_dag(verbose=False)
         print(self.dag)
 
     def printrulegraph(self):
@@ -979,7 +979,7 @@ class Workflow(WorkflowExecutorInterface):
             ignore_incomplete=True,
             lock_warn_only=True,
         )
-        self._build_dag()
+        self._build_dag(verbose=False)
         print(self.dag.rule_dot())
 
     def printfilegraph(self):
@@ -989,7 +989,7 @@ class Workflow(WorkflowExecutorInterface):
             ignore_incomplete=True,
             lock_warn_only=True,
         )
-        self._build_dag()
+        self._build_dag(verbose=False)
         print(self.dag.filegraph_dot())
 
     def printd3dag(self):
@@ -999,7 +999,7 @@ class Workflow(WorkflowExecutorInterface):
             ignore_incomplete=True,
             lock_warn_only=True,
         )
-        self._build_dag()
+        self._build_dag(verbose=False)
 
         self.dag.d3dag()
 
@@ -1160,8 +1160,9 @@ class Workflow(WorkflowExecutorInterface):
             rulegraph = simple_rulegraph()
             logger.info(None, extra=dict(event=LogEvent.RULEGRAPH, rulegraph=rulegraph))
 
-    def _build_dag(self):
-        logger.info("Building DAG of jobs...")
+    def _build_dag(self, verbose=True):
+        if verbose:
+            logger.info("Building DAG of jobs...")
         async_run(self.dag.init())
         async_run(self.dag.update_checkpoint_dependencies())
 
