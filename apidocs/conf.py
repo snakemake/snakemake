bf636aef3e1b12fa1e90380aa3861a49c7f3ee53
Johannes Koester <johannes.koester@uni-due.de>
2025-07-02 14:00:30 +0200

    fix

--- apidocs/conf.py
+++ apidocs/conf.py
@@ -191,7 +191,7 @@ html_static_path = ["../docs/_static"]
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {"ethicalads.html"}
+html_sidebars = {"**": ["ethicalads.html"]}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
