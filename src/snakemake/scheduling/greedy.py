bb713b1ee41acfbc4c30f83a27858a2227aaf984
Johannes Koester <johannes.koester@uni-due.de>
2025-07-18 14:08:14 +0200

    fixes

--- src/snakemake/scheduling/greedy.py
+++ src/snakemake/scheduling/greedy.py
@@ -3,7 +3,7 @@ from itertools import filterfalse
 from typing import Dict, Iterable, Mapping, Optional, Sequence, Union
 from snakemake_interface_scheduler_plugins.base import SchedulerBase
 from snakemake_interface_scheduler_plugins.settings import SchedulerSettingsBase
-from snakemake_interface_scheduler_plugins.interfaces.job import JobSchedulerInterface
+from snakemake_interface_scheduler_plugins.interfaces.jobs import JobSchedulerInterface
 from snakemake_interface_common.io import AnnotatedStringInterface
 
 class Settings(SchedulerSettingsBase):
