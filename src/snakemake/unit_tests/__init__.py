2de8b3b408700365f7fd2a65d7bfa9776e159c98
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-07-25 13:30:31 +0200

    Format code

--- src/snakemake/unit_tests/__init__.py
+++ src/snakemake/unit_tests/__init__.py
@@ -28,7 +28,9 @@ class RuleTest:
         return self.path / "expected"
 
 
-def generate(dag, path: Path, deploy=["conda", "singularity"], snakefile=None, configfiles=None):
+def generate(
+    dag, path: Path, deploy=["conda", "singularity"], snakefile=None, configfiles=None
+):
     """Generate unit tests from given dag at a given path."""
     logger.info("Generating unit tests for each rule...")
 
