75b1cabf1d975359117759bc7d1b309ab36b7bec
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-08-04 13:47:25 +0200

    Small tweaks

--- tests/test_generate_unit_tests/expected-results/.tests/unit/common.py
+++ tests/test_generate_unit_tests/expected-results/.tests/unit/common.py
@@ -1,10 +1,10 @@
 """
-Common code for unit testing of rules generated with Snakemake 9.8.2.dev35.
+Common code for unit testing of rules generated with Snakemake 9.8.0.
 """
 
-import os
-import subprocess as sp
 from pathlib import Path
+import subprocess as sp
+import os
 
 
 class OutputChecker:
@@ -14,42 +14,35 @@ class OutputChecker:
         self.workdir = workdir
 
     def check(self):
-        # Input files
         input_files = set(
             (Path(path) / f).relative_to(self.data_path)
             for path, subdirs, files in os.walk(self.data_path)
             for f in files
         )
-        print(f"input: {input_files}")  # DEBUG
-        # Workdir files
-        workdir_files = set(
-            (Path(path) / f).relative_to(self.workdir)
-            for path, subdirs, files in os.walk(self.workdir)
-            for f in files
-        )
-        print(f"workdir: {workdir_files}")  # DEBUG
-        # Expected files
         expected_files = set(
             (Path(path) / f).relative_to(self.expected_path)
             for path, subdirs, files in os.walk(self.expected_path)
             for f in files
         )
-        print(f"expected: {expected_files}")  # DEBUG
+        unexpected_files = set()
+        for path, subdirs, files in os.walk(self.workdir):
+            for f in files:
+                f = (Path(path) / f).relative_to(self.workdir)
+                if str(f).startswith(".snakemake"):
+                    continue
+                if f in expected_files:
+                    self.compare_files(self.workdir / f, self.expected_path / f)
+                elif f in input_files:
+                    # ignore input files
+                    pass
+                else:
+                    unexpected_files.add(f)
+        if unexpected_files:
+            raise ValueError(
+                "Unexpected files:\n{}".format(
+                    "\n".join(sorted(map(str, unexpected_files)))
+                )
+            )
 
-        assert expected_files.issubset(
-            workdir_files
-        ), f"Output files missing: {expected_files - workdir_files}"
-
-        # Compare output and expected files
-        for f in expected_files:
-            self.compare_files(self.expected_path / f, self.workdir / f)
-
-    def compare_files(self, expected_file, generated_file):
-        if expected_file.suffix == ".gz":
-            cmp = "zcmp"
-        elif expected_file.suffix == ".bz2":
-            cmp = "bzcmp"
-        else:
-            cmp = "cmp"
-
-        sp.check_output([cmp, expected_file, generated_file])
+    def compare_files(self, generated_file, expected_file):
+        sp.check_output(["cmp", generated_file, expected_file])
--- tests/test_generate_unit_tests/expected-results/.tests/unit/test_b.py
+++ tests/test_generate_unit_tests/expected-results/.tests/unit/test_b.py
@@ -1,57 +1,45 @@
-"""
-Rule test code for unit testing of rules generated with Snakemake 9.8.2.dev35.
-"""
-
-
 import os
 import sys
-import shutil
-import tempfile
+
 import subprocess as sp
-from pathlib import Path
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+import common
 
-def test_b(conda_prefix):
 
-    with tempfile.TemporaryDirectory() as tmpdir:
+def test_b():
+
+    with TemporaryDirectory() as tmpdir:
         workdir = Path(tmpdir) / "workdir"
-        data_path = Path(".tests/unit/b/data")
-        expected_path = Path(".tests/unit/b/expected")
+        data_path = PurePosixPath(".tests/unit/b/data")
+        expected_path = PurePosixPath(".tests/unit/b/expected")
 
         # Copy data to the temporary workdir.
         shutil.copytree(data_path, workdir)
 
-        # Copy config/ (if exists) to the temporary workdir.
-        config = Path("config")
-        if config.exists() and config.is_dir():
-            shutil.copytree("config", workdir / config, dirs_exist_ok=True)
+        # dbg
+        print("test/0.tsv", file=sys.stderr)
 
         # Run the test job.
-        sp.check_output(
-            [
-                "python",
-                "-m",
-                "snakemake",
-                "test/0.tsv",
-                "--snakefile",
-                "Snakefile",
-                "-f",
-                "--notemp",
-                "-j1",
-                "--target-files-omit-workdir-adjustment",
-                "--configfile",
-                "config/config.json",
-                "--directory",
-                workdir,
-            ]
-            + conda_prefix
-        )
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "test/0.tsv",
+            "-f", 
+            "-j1",
+            "--target-files-omit-workdir-adjustment",
+    
+            "--directory",
+            workdir,
+        ])
 
-        # Check the output byte by byte using cmp/zmp/bzcmp.
+        # Check the output byte by byte using cmp.
         # To modify this behavior, you can inherit from common.OutputChecker in here
         # and overwrite the method `compare_files(generated_file, expected_file), 
         # also see common.py.
-        import common
         common.OutputChecker(data_path, expected_path, workdir).check()
