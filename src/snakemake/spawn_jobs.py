b79182a934b342f4810a531a5b1ebde4dc2db869
Peter Van Dyken <pvandyk2@uwo.ca>
2025-11-03 16:39:04 -0500

    fix:standardize handling of resources
    This centralizes the code for resource handling into the resource.py
    file under a new Resource class. Notably, DefaultResources has been
    removed, it's functionality rolled into the new class. The same class is
    now used for handling global resource limits, default resources, and
    rule resource constraints
    
    As a result of this standarization, the following bugs/inconsistencies are fixed:
    
    1) humanreadable resources can now be specified in a group job in
       cluster submit without crashing the program
    2) if mem or disk are used as global resource limits, they will
       correctly restrict mem_mb, mem_mib, disk_mb, and disk_mib
    3) similar to above when setting a global scope for mem and disk
    4) humanreadable resources can now be used for global resource limits
    5) disk_mib and mem_mib can be set in rule resource specifications,
       global limits, and in default resources, and will correctly set the
       _mb variants.
    6) Providing the same effective resource twice (e.g. mem, mem_mb and/or
       mem_mib) in the same rule or in the global resource specification
       raises an error.
    7) QOL: Resources in tests can be set using mappings
    8) Runtime is handled as a human-readable resource
    9) Original raw resource specifications are maintained to pass on to
       subprocesses
    
    10) Fix handling of input-based resource lambdas
    
    Previously, lambdas with an input parameter were skipped by inspecting
    the arguments of the lambda via introspection. This did not work well
    with the new refactor, which involves potentially deep nesting of these
    lambdas. Instead, always run these input functions, relying on the fact
    that missing input files will already result in TBDStrings, the same
    outcome as the previous skip_evaluation.
    
    Currently limited by second bug, where input file sizes cannot be
    evaulated in the initial dag creation routine due to nesting of
    asynchronous routines. For now, simply catch these nesting errors and
    routine TBDString when they occur
    
    11) Move resource expansion logic to resources.py
    
    Better separation of concerns: in particular, handling of "magic"
    resources "mem" and "disk" and their suffixed versions is handled
    totally within `Resources`. `Rule.expand_resources` now only has logic
    that requires direct knowledge of the rule.
    
    12) Move humanfriendly from expr to main parse block
    
    Previously only evaluated in the context of epxressions, preventing use
    of human friendly resources with --resources. No reason for this, as
    they can be parsed deterministically.
    
    13) Sized resources are stored without a suffix
    
    This makes much of the logic more straightforward, and facilitates
    allowing callables on mem and disk to return strings which can then be
    parsed as human-readable
    
    14) Remove positive_integer arguments, CLI should always validate
        that integers are positive
    
    15) Exclude the string versions of sized resources from the group layer
        function, then add in after

--- src/snakemake/spawn_jobs.py
+++ src/snakemake/spawn_jobs.py
@@ -6,7 +6,6 @@ import sys
 from typing import Callable, Mapping, TypeVar, TYPE_CHECKING, Any
 from snakemake_interface_executor_plugins.utils import format_cli_arg, join_cli_args
 from snakemake_interface_executor_plugins.settings import CommonSettings
-from snakemake.resources import ParsedResource
 from snakemake_interface_storage_plugins.registry import StoragePluginRegistry
 
 from snakemake import PIP_DEPLOYMENTS_PATH
@@ -96,17 +95,11 @@ class SpawnedJobArgsFactory:
         }
 
     def get_set_resources_args(self):
-        def get_orig_arg(value):
-            if isinstance(value, ParsedResource):
-                return value.orig_arg
-            else:
-                return value
-
         return [
             format_cli_arg(
                 "--set-resources",
                 [
-                    f"{rule}:{name}={get_orig_arg(value)}"
+                    f"{rule}:{name}={value.raw}"
                     for rule, res in self.workflow.resource_settings.overwrite_resources.items()
                     for name, value in res.items()
                 ],
@@ -116,7 +109,7 @@ class SpawnedJobArgsFactory:
             format_cli_arg(
                 "--set-threads",
                 [
-                    f"{rule}={get_orig_arg(value)}"
+                    f"{rule}={value.raw}"
                     for rule, value in self.workflow.resource_settings.overwrite_threads.items()
                 ],
                 skip=not self.workflow.resource_settings.overwrite_threads,
