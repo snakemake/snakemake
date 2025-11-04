f39ef16add1da43dcc4c34a8cfc9f005f0a4958d
Johannes Koester <johannes.koester@uni-due.de>
2025-11-04 14:44:52 +0100

    fix: correctly handle meta-wrapper tag replacement depending on the used snakemake-wrapper release

--- src/snakemake/modules.py
+++ src/snakemake/modules.py
@@ -127,13 +127,17 @@ class ModuleInfo:
             )
 
     def get_wrapper_tag(self):
+        from packaging.version import Version
+
         if self.meta_wrapper:
             if wrapper.is_url(self.meta_wrapper):
                 # no wrapper tag replacement, use meta-wrapper as is
                 return None
             tag = self.meta_wrapper.split("/", 1)[0]
-            
-            tag.rstrip("v").split(".")
+            ver_match = wrapper.ver_regex.match(tag)
+            if ver_match and Version(ver_match.group("ver")) >= Version("8.0.0"):
+                # New style meta-wrappers, containing concrete versions of each wrapper
+                return None
         return None
 
     def get_rule_whitelist(self, rules):
