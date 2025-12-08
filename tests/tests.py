5a5cfc44e7238432007f943f168d0f569a223463
Johannes Koester <johannes.koester@uni-due.de>
2025-12-08 11:43:15 +0100

    fix test

--- tests/tests.py
+++ tests/tests.py
@@ -2660,10 +2660,12 @@ def test_wildcard_annotatedstrings():
         run(dpath("test_wildcard_annotatedstrings"), targets=["test.out"])
 
 
+@skip_on_windows  # platform will have no effect
 def test_cyclic_dependency_split():
     run(dpath("test_cyclic_dependency_split"))
 
 
+@skip_on_windows  # platform will have no effect
 def test_cyclic_dependency_single():
     # We force a rerun because the (to be updated) output file is already there
     # and there is no input file with a newer date.
