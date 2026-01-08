0bdef5140160fc4c197e7ed30502aa8598e56948
Johannes Koester <johannes.koester@uni-due.de>
2026-01-08 12:32:40 +0100

    fix lock-free clone

--- src/snakemake/sourcecache.py
+++ src/snakemake/sourcecache.py
@@ -322,14 +322,24 @@ class HostedGitRepo:
             with self._tmpdir() as tmpdir:
                 # the clone is not atomic, hence we do that in a temporary directory
                 # We only want the database, thus we create a bare clone
-                Repo.clone_from(repo_url, to_path=tmpdir, bare=True)
-                # move is atomic if repo_clone does not exist, so we can safely move
-                # the directory to the final location
-                shutil.move(tmpdir, self.repo_clone)
+                try:
+                    Repo.clone_from(repo_url, to_path=tmpdir, bare=True)
+                except Exception:
+                    # clean up on any exception
+                    shutil.rmtree(tmpdir)
+                    raise
+                try:
+                    # move is atomic if repo_clone does not exist, so we can safely move
+                    # the directory to the final location
+                    os.rename(tmpdir, self.repo_clone)
+                except FileExistsError:
+                    # another process won the race
+                    shutil.rmtree(tmpdir)
+
             self._repo = Repo(self.repo_clone)
 
     def _tmpdir(self) -> tempfile.TemporaryDirectory:
-        return tempfile.TemporaryDirectory(prefix=f"{self.repo_clone}.")
+        return tempfile.TemporaryDirectory(prefix=f"{self.repo_clone}.", delete=False)
 
     def ref_exists(self, ref: str):
         import git
