66e7626d990d1fff011ba0f737acb0746ba459fa
sof202 <s.o.fletcher@exeter.ac.uk>
2026-02-05 12:12:06 +0000

    fix: Create actual instance of validator from validator_for
    Suggestion by CodeRabbit. I must admit, I didn't see this myself.
    `validator_for` returns a class, not an instance. My bad

--- src/snakemake/utils.py
+++ src/snakemake/utils.py
@@ -116,6 +116,7 @@ def validate(data, schema, set_default=True):
         uri=schemafile.get_path_or_uri(secret_free=False), resource=resource
     )
     Validator = validator_for(schema, default=Draft202012Validator)
+    ValidatorInstance = Validator(schema, registry=registry)
 
     # Taken from https://python-jsonschema.readthedocs.io/en/latest/faq/
     def extend_with_default(validator_class):
@@ -154,7 +155,7 @@ def validate(data, schema, set_default=True):
             Defaultvalidator(schema, registry=registry).validate(record)
             return record
         else:
-            Validator.validate(record)
+            ValidatorInstance.validate(record)
 
     def _validate_pandas(data):
         try:
