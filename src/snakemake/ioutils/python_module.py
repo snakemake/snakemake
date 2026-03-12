f9d9b0b7ccf693321c91dec16604cbc887779683
Ed Bennett <e.j.bennett@swansea.ac.uk>
2026-03-12 10:27:15 +0100

    use pathlib rather than reinventing path parsing

--- src/snakemake/ioutils/python_module.py
+++ src/snakemake/ioutils/python_module.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import re
 
 
@@ -9,14 +10,14 @@ def format_python_module(filename: str) -> str:
     return the name of the corresponding module that can be passed to `python -m`.
     For example,
 
-    >>> python_module("package/subpackage/module.py")
+    >>> format_python_module("package/subpackage/module.py")
     'package.subpackage.module'
     """
 
     if not filename.endswith(".py"):
         raise ValueError("Only .py files may be run as Python modules.")
 
-    components = filename[:-3].replace("\\", "/").split("/")
+    components = Path(filename[:-3]).parts
     if any(
         re.match("[A-Za-z_][A-Za-z0-9_]*$", component) is None
         for component in components
