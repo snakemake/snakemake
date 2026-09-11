c561a0439b096898792b3705d722ab882dafe354
Johannes Koester <johannes.koester@uni-due.de>
2026-09-11 07:57:36 +0200

    fix

--- tests/tests.py
+++ tests/tests.py
@@ -997,12 +997,11 @@ def test_container_a():
 
 @skip_on_windows
 @connected
-@apptainer
+@skip_on_macos
 def test_container_cluster():
     run(
         dpath("test_singularity"),
         deployment_method={"container"},
-        container_runtime="apptainer",
         cluster="./qsub",
     )
 
